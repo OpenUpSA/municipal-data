ac83d72bde4bcfc63be62cb3d13c9272d6226c25
JD Bothma <jbothma@gmail.com>
2016-04-04 18:28:51 +0200

    add url mappings for rest of API dumbly

--- municipal_finance/municipal_finance/urls.py
+++ municipal_finance/municipal_finance/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^cubes$', views.cubes),
+    url(r'^cubes/<name>/model$', views.model),
+    url(r'^cubes/<name>/aggregate$', views.agregate),
+    url(r'^cubes/<name>/facts$', views.facts),
+    url(r'^cubes/<name>/members/<ref>$', views.members),
 ]
