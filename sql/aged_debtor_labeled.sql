48ca85a8cd6c6f42cb5715440c9dea3e7849be84
JD Bothma <jbothma@gmail.com>
2016-04-12 10:19:32 +0200

    add aged debtor analysis table

---
+++ sql/aged_debtor_labeled.sql
@@ -0,0 +1,37 @@
+-- Table: public.aged_debtor_labeled
+
+-- DROP TABLE public.aged_debtor_labeled;
+
+CREATE TABLE public.aged_debtor_labeled
+(
+  demarcation_code text,
+  period_code text,
+  customer_grp_cde text,
+  age_cde text,
+  bad_amount bigint,
+  badi_amount bigint,
+  g1_amount bigint,
+  l1_amount bigint,
+  l120_amount bigint,
+  l150_amount bigint,
+  l180_amount bigint,
+  l30_amount bigint,
+  l60_amount bigint,
+  l90_amount bigint,
+  total_amount bigint,
+  function_desc text,
+  aged_debtor_desc text,
+  id serial,
+  demarcation_desc text,
+  financial_year text,
+  period_length text,
+  financial_period text,
+  amount_type_cde text,
+  amount_type_desc text,
+  CONSTRAINT aged_debtor_labeled_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.aged_debtor_labeled
+  OWNER TO municipal_finance;
---
+++ sql/aged_debtor_to_labeled.sql
@@ -0,0 +1,10 @@
+update aged_debtor_labeled set financial_year = left(period_code, 4);
+update aged_debtor_labeled set amount_type_cde = substr(period_code, 5) where substr(period_code, 5) in ('IBY1', 'IBY2', 'ADJB', 'ORGB', 'AUDA', 'PAUD', 'IBY2');
+update aged_debtor_labeled set amount_type_cde = 'ACT' where amount_type_cde is null;
+update aged_debtor_labeled set aged_debtor_desc = subquery.description from (select * from codes_desc ) as subquery where subquery.column_name = 'AGE_CDE' and subquery.code = age_cde;
+update aged_debtor_labeled set demarcation_desc = sub.name from (select * from demarcation) sub where sub.code = demarcation_code;
+update aged_debtor_labeled set period_length = 'year' where amount_type_cde != 'ACT';
+update aged_debtor_labeled set period_length = 'month' where amount_type_cde = 'ACT';
+update aged_debtor_labeled set financial_period = right(period_code, 2) where period_length = 'month';
+update aged_debtor_labeled set financial_period = left(period_code, 4) where period_length = 'year';
+update aged_debtor_labeled set amount_type_desc = sub.name from (select * from amount_type) as sub where sub.code = amount_type_cde;
