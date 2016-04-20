78045fffc3026548dfbe9b8a2e9981389ab26802
Greg Kempe <greg@kempe.net>
2016-04-20 14:55:33 +0200

    Use wazimap-mapit

--- municipal_finance/geo.py
+++ municipal_finance/geo.py
@@ -1,19 +1,19 @@
-from wazimap.geo import GeoData as GeoDataBase
+from wazimap_mapit.geo import GeoData as GeoDataBase
 
 
 class GeoData(GeoDataBase):
     LEVELS = ['municipality', 'district']
 
-    def get_locations(self, search_term, levels=None, year=None):
-        # ensure we're limiting levels
+    def _clean_levels(self, levels):
         if not levels:
-            levels = self.LEVELS
+            return self.LEVELS
         else:
-            levels = set(levels) & set(self.LEVELS)
-        levels = ','.join(levels)
+            return list(set(levels) & set(self.LEVELS))
 
+    def get_locations(self, search_term, levels=None, year=None):
+        levels = self._clean_levels(levels)
         return super(GeoData, self).get_locations(search_term, levels, year)
 
-    def get_locations_from_coords(self, longitude, latitude):
-        geos = super(GeoData, self).get_locations_from_coords(longitude, latitude)
-        return [g for g in geos if g.level in self.LEVELS]
+    def get_locations_from_coords(self, longitude, latitude, levels=None):
+        levels = self._clean_levels(levels)
+        return super(GeoData, self).get_locations_from_coords(longitude, latitude, levels)
