1393a9e83beaa9a837991ce01893e82b4c208dc1
Damian <damian@code4sa.org>
2016-04-26 14:18:23 +0200

    Add Repairs and Maintenance as a % of PPE

--- scorecard/profiles.py
+++ scorecard/profiles.py
@@ -12,8 +12,6 @@ def get_profile(geo_code, geo_level, profile_name=None):
 
     api_query_string = '{cube}/aggregate?aggregates={aggregate}&cut={cut}&drilldown=item.code|item.label|financial_period.period&page=0&pagesize=300000'
 
-    # item.code:"{item_code}"|amount_type.label:"{amount_type}"|financial_year_end.year:{year}|demarcation.code:"{demarcation_code}"|period_length.length:"{period_length}"
-
     line_items = {
         'op_exp_actual': {
             'cube': 'incexp',
@@ -122,6 +120,41 @@ def get_profile(geo_code, geo_level, profile_name=None):
                 'demarcation.code': str(geo_code),
                 'period_length.length': 'month'
             }
+        },
+        'rep_maint': {
+            'cube': 'repmaint',
+            'aggregate': 'amount.sum',
+            'cut': {
+                'item.code': '5005',
+                'amount_type.label': 'Actual',
+                'financial_year_end.year': 2015,
+                'demarcation.code': str(geo_code),
+                'period_length.length': 'month'
+            }
+        },
+        'ppe': {
+            'cube': 'bsheet',
+            'aggregate': 'amount.sum',
+            'cut': {
+                'item.code': '1300',
+                'amount_type.label': 'Actual',
+                'financial_year_end.year': 2015,
+                'demarcation.code': str(geo_code),
+                'period_length.length': 'month',
+                'financial_period.period': current_month
+            }
+        },
+        'invest_prop': {
+            'cube': 'bsheet',
+            'aggregate': 'amount.sum',
+            'cut': {
+                'item.code': '1401',
+                'amount_type.label': 'Actual',
+                'financial_year_end.year': 2015,
+                'demarcation.code': str(geo_code),
+                'period_length.length': 'month',
+                'financial_period.period': current_month
+            }
         }
     }
 
@@ -148,6 +181,7 @@ def get_profile(geo_code, geo_level, profile_name=None):
     cash_flow = [
         r['amount.sum'] for r in results['cash_flow']['cells']
         if r['financial_period.period'] == current_month][0]
+
     debtors = [
         r['total_amount.sum'] for r in results['debtors']['cells']
         if r['financial_period.period'] == current_month][0]
@@ -155,13 +189,19 @@ def get_profile(geo_code, geo_level, profile_name=None):
     cap_grant = get_quarter_results(results['cap_grant'])
     op_grant = get_quarter_results(results['op_grant'])
 
-    debtors_perc_rev = debtors/((cap_rev+op_rev) - (cap_grant+op_grant))*100
-    cash_coverage = cash_flow/(op_exp_actual/12)
-    op_budget_diff = (op_exp_actual-op_exp_budget)/op_exp_budget if op_exp_budget else 0
-    cap_budget_diff = (cap_exp_actual-cap_exp_budget)/cap_exp_budget if cap_exp_budget else 0
+    rep_maint = get_quarter_results(results['rep_maint'])
+    ppe = results['ppe']['cells'][0][line_items['ppe']['aggregate']]
+    invest_prop = results['invest_prop']['cells'][0][line_items['ppe']['aggregate']]
+
+    debtors_perc_rev = debtors / ((cap_rev+op_rev) - (cap_grant + op_grant)) * 100
+    cash_coverage = cash_flow / (op_exp_actual / 12)
+    op_budget_diff = (op_exp_actual - op_exp_budget) / op_exp_budget if op_exp_budget else 0
+    cap_budget_diff = (cap_exp_actual - cap_exp_budget) / cap_exp_budget if cap_exp_budget else 0
+    rep_maint_perc_ppe = rep_maint * 12 / (ppe + invest_prop)
 
     return {
         'cash_coverage': cash_coverage,
         'debtors_perc_rev': debtors_perc_rev,
         'op_budget_diff': op_budget_diff,
-        'cap_budget_diff': cap_budget_diff}
+        'cap_budget_diff': cap_budget_diff,
+        'rep_maint_perc_ppe': rep_maint_perc_ppe}
