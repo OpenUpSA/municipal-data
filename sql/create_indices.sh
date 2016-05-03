21b42895214a9680de904f554777e3b51772bebc
JD Bothma <jbothma@gmail.com>
2016-05-03 16:57:16 +0200

    revert host changes

--- sql/create_indices.sh
+++ sql/create_indices.sh
@@ -1,11 +1,11 @@
-for cube in `curl  http://172.17.0.2:8000/api/cubes|jq -r .data[].name`
+for cube in `curl  http://localhost:8000/api/cubes|jq -r .data[].name`
 do
-    TABLE=`curl http://172.17.0.2:8000/api/cubes/$cube/model|jq -r '.model.fact_table'`
-    for dimension in `curl http://172.17.0.2:8000/api/cubes/$cube/model|jq -r '.model.dimensions|keys[]'`
+    TABLE=`curl http://localhost:8000/api/cubes/$cube/model|jq -r '.model.fact_table'`
+    for dimension in `curl http://localhost:8000/api/cubes/$cube/model|jq -r '.model.dimensions|keys[]'`
     do
-        for attribute in `curl http://172.17.0.2:8000/api/cubes/$cube/model|jq -r ".model.dimensions.$dimension.attributes|keys[]"`
+        for attribute in `curl http://localhost:8000/api/cubes/$cube/model|jq -r ".model.dimensions.$dimension.attributes|keys[]"`
         do
-            COL=`curl http://172.17.0.2:8000/api/cubes/$cube/model|jq -r ".model.dimensions.$dimension.attributes.$attribute.column"`
+            COL=`curl http://localhost:8000/api/cubes/$cube/model|jq -r ".model.dimensions.$dimension.attributes.$attribute.column"`
             echo "create index if not exists ${TABLE}_${COL}_idx on $TABLE ($COL);"
         done
     done
