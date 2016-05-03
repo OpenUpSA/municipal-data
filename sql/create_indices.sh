acd643f3827c3e6ecdc4fe5e549651f6a2d5b24f
JD Bothma <jbothma@gmail.com>
2016-05-03 16:48:38 +0200

    Add indices for audit_opinions

--- sql/create_indices.sh
+++ sql/create_indices.sh
@@ -1,12 +1,12 @@
-for cube in `curl  http://localhost:8000/api/cubes|jq -r .data[].name`
+for cube in `curl  http://172.17.0.2:8000/api/cubes|jq -r .data[].name`
 do
-    TABLE=`curl http://localhost:8000/api/cubes/$cube/model|jq -r '.model.fact_table'`
-    for dimension in `curl http://localhost:8000/api/cubes/$cube/model|jq -r '.model.dimensions|keys[]'`
+    TABLE=`curl http://172.17.0.2:8000/api/cubes/$cube/model|jq -r '.model.fact_table'`
+    for dimension in `curl http://172.17.0.2:8000/api/cubes/$cube/model|jq -r '.model.dimensions|keys[]'`
     do
-        for attribute in `curl http://localhost:8000/api/cubes/$cube/model|jq -r ".model.dimensions.$dimension.attributes|keys[]"`
+        for attribute in `curl http://172.17.0.2:8000/api/cubes/$cube/model|jq -r ".model.dimensions.$dimension.attributes|keys[]"`
         do
-            COL=`curl http://localhost:8000/api/cubes/$cube/model|jq -r ".model.dimensions.$dimension.attributes.$attribute.column"`
-            echo "create index on $TABLE ($COL);"
+            COL=`curl http://172.17.0.2:8000/api/cubes/$cube/model|jq -r ".model.dimensions.$dimension.attributes.$attribute.column"`
+            echo "create index if not exists ${TABLE}_${COL}_idx on $TABLE ($COL);"
         done
     done
 done
