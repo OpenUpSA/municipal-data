d245084ab9c004f94c99970b849710b327a0185a
JD Bothma <jbothma@gmail.com>
2016-05-09 18:04:55 +0200

    Add grants table definition and model

---
+++ sql/grants_labeled.sql
@@ -0,0 +1,25 @@
+-- Table: public.grants_labeled
+
+-- DROP TABLE public.grants_labeled;
+
+CREATE TABLE public.grants_labeled
+(
+  demacation_code text,
+  period_code text,
+  grant_code text,
+  amount integer,
+  grant_name text,
+  amount_type_code text,
+  id integer NOT NULL DEFAULT nextval('grants_labeled_id_seq'::regclass),
+  demarcation_label text,
+  financial_year integer,
+  period_length text,
+  financial_period integer,
+  amount_type_label text,
+  CONSTRAINT grants_labeled_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.grants_labeled
+  OWNER TO postgres;
