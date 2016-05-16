2024285ee0f28d4959e1fb9ac01968a9625eb21a
Damian <damian@code4sa.org>
2016-05-16 12:52:12 +0200

    Add population table + numbers

---
+++ scorecard/tables.py
@@ -0,0 +1,10 @@
+from wazimap.data.tables import SimpleTable
+
+SimpleTable(
+    id='population',
+    universe='Population',
+    total_column=None,
+    description='Total population',
+    dataset='Census 2011',
+    year='2011'
+)
