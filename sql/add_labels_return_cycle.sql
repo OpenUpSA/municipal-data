89c31f675e68cc58e28c1535a2387b79fbc5192d
JD Bothma <jbothma@gmail.com>
2016-05-17 17:01:04 +0200

    period code breakdown

--- sql/add_labels_return_cycle.sql
+++ sql/add_labels_return_cycle.sql
@@ -1,7 +1,7 @@
-update incexp_big set financial_year = cast(left(period_code, 4) as int);
-update incexp_big set amount_type_code = substr(period_code, 5) where substr(period_code, 5) in ('IBY1', 'IBY2', 'ADJB', 'ORGB', 'AUDA', 'PAUD', 'IBY2');
-update incexp_big set amount_type_code = 'ACT' where substr(period_code, 5) not in ('IBY1', 'IBY2', 'ADJB', 'ORGB', 'AUDA', 'PAUD', 'IBY2');
-update incexp_big set period_length = 'year' where substr(period_code, 5, 3) not similar to 'M\d{2}';
-update incexp_big set period_length = 'month' where substr(period_code, 5, 3) similar to 'M\d{2}';
-update incexp_big set financial_period = cast(right(period_code, 2) as int) where period_length = 'month';
-update incexp_big set financial_period = cast(left(period_code, 4) as int) where period_length = 'year';
+update incexp_facts set financial_year = cast(left(period_code, 4) as int);
+update incexp_facts set amount_type_code = substr(period_code, 5) where substr(period_code, 5) in ('IBY1', 'IBY2', 'ADJB', 'ORGB', 'AUDA', 'PAUD', 'IBY2');
+update incexp_facts set amount_type_code = 'ACT' where substr(period_code, 5) not in ('IBY1', 'IBY2', 'ADJB', 'ORGB', 'AUDA', 'PAUD', 'IBY2');
+update incexp_facts set period_length = 'year' where substr(period_code, 5, 3) not similar to 'M\d{2}';
+update incexp_facts set period_length = 'month' where substr(period_code, 5, 3) similar to 'M\d{2}';
+update incexp_facts set financial_period = cast(right(period_code, 2) as int) where period_length = 'month';
+update incexp_facts set financial_period = cast(left(period_code, 4) as int) where period_length = 'year';
