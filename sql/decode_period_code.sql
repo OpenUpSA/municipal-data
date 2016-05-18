dd51de9ca8c1bbcfe975f60415dc1242cb0f29e6
JD Bothma <jbothma@gmail.com>
2016-05-18 13:12:01 +0200

    fix grants amount type codes

--- sql/decode_period_code.sql
+++ sql/decode_period_code.sql
@@ -46,8 +46,8 @@ update cflow_facts set financial_period = cast(left(period_code, 4) as int) wher
 
 \echo 'conditional_grants'
 update conditional_grants_facts set financial_year = cast(left(period_code, 4) as int);
-update conditional_grants_facts set amount_type_code = substr(period_code, 5) where substr(period_code, 5) in ('IBY1', 'IBY2', 'ADJB', 'ORGB', 'AUDA', 'PAUD', 'IBY2', 'TRFR', 'SCHD');
-update conditional_grants_facts set amount_type_code = 'ACT' where substr(period_code, 5) not in ('IBY1', 'IBY2', 'ADJB', 'ORGB', 'AUDA', 'PAUD');
+update conditional_grants_facts set amount_type_code = substr(period_code, 5) where substr(period_code, 5) in ('IBY1', 'IBY2', 'ADJB', 'ORGB', 'AUDA', 'PAUD', 'TRFR', 'SCHD');
+update conditional_grants_facts set amount_type_code = 'ACT' where substr(period_code, 5) not in ('IBY1', 'IBY2', 'ADJB', 'ORGB', 'AUDA', 'PAUD', 'TRFR', 'SCHD');
 update conditional_grants_facts set period_length = 'year' where substr(period_code, 5, 3) not similar to 'M\d{2}';
 update conditional_grants_facts set period_length = 'month' where substr(period_code, 5, 3) similar to 'M\d{2}';
 update conditional_grants_facts set financial_period = cast(right(period_code, 2) as int) where period_length = 'month';
