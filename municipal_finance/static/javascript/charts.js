9556fb2a5be257081f0ba3f160d6a86376276015
Damian <damian@code4sa.org>
2016-05-24 17:52:29 +0200

    Simple chart

--- municipal_finance/static/javascript/charts.js
+++ municipal_finance/static/javascript/charts.js
@@ -2,6 +2,52 @@ var Chart = function() {
   var self = this;
 
   self.init = function() {
+    var data = DATA;
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 500 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .9);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var svg = d3.select(".chart-container").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(data.map(function(d) { return d.year; }));
+    y.domain([0, d3.max(data, function(d) { return d.result; })]);
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    svg.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "chart-bar")
+        .attr("x", function(d) { return x(d.year); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.result); })
+        .attr("height", function(d) { return height - y(d.result); })
+        .attr("fill", function(d) {
+          if (d.rating == 'good') {
+            return "green";
+          } else if (d.rating == 'bad') {
+            return "red";
+          }
+          return "yellow";
+        });
   }
 }
 
