c6f599f3152c09ab1a6caf0447c1fe2a09fec5c1
JD Bothma <jbothma@gmail.com>
2016-05-27 13:46:00 +0200

    Proper app init ready hook for cube_manager initialisation

--- municipal_finance/__init__.py
+++ municipal_finance/__init__.py
@@ -1,2 +1,12 @@
-# Unused import to initialise cube_manager
-import cubes
+from django.apps import AppConfig
+
+default_app_config = 'municipal_finance.Config'
+
+
+class Config(AppConfig):
+    name = 'municipal_finance'
+    verbose_name = 'Municipal Finance API'
+
+    def ready(self):
+        # Unused import to initialise cube_manager
+        import cubes
