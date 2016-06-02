194e5229c064f32c3ffa9e94b0374ddb2c8e8e7e
Greg Kempe <greg@kempe.net>
2016-06-02 15:52:18 +0200

    Locate view

--- scorecard/urls.py
+++ scorecard/urls.py
@@ -3,18 +3,14 @@ from django.views.decorators.cache import cache_page
 from django.views.generic.base import TemplateView
 from wazimap.views import GeographyDetailView, PlaceSearchJson
 
+import scorecard.views as views
+
 # This cache is reset on each deployment. Corresponding caching headers are
 # sent to the client, too.
 CACHE_SECS = 12 * 60 * 60
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='homepage'),
-
-    # temporary path for developing the html version of the mockup
-    url(regex   = r'^profiles/\w+-\w+-mockup?/$',
-        view    = TemplateView.as_view(template_name='profile/profile_detail_mockup.html'),
-        kwargs  = {},
-        name    = 'geography_detail'),
 
     # e.g. /profiles/province-GT/
     url(
@@ -28,5 +24,11 @@ urlpatterns = [
         view    = cache_page(CACHE_SECS)(PlaceSearchJson.as_view()),
         kwargs  = {},
         name    = 'place_search_json',
+    ),
+    url(
+        regex   = '^locate/$',
+        view    = cache_page(CACHE_SECS)(views.locate),
+        kwargs  = {},
+        name    = 'locate',
     ),
 ]
