b764b5095da5ab0968fbbf0a7d0d3cb212b123f6
Greg Kempe <greg@kempe.net>
2016-06-02 14:34:14 +0200

    Clickable formulas

---
+++ scorecard/templatetags/table.py
@@ -0,0 +1,26 @@
+import urllib
+
+from django import template
+from django.conf import settings
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def table_url(context, cube, year, muni=None, items=None):
+    if not muni:
+        muni = context['geography']['this']['geo_code']
+
+    params = {
+        "year": year,
+        "municipalities": muni,
+    }
+    if items:
+        params["items"] = items
+
+    for k, v in params.iteritems():
+        if isinstance(v, list):
+            params[k] = ",".join(v)
+
+    return settings.API_BASE + "/table/" + cube + "/?" + urllib.urlencode(params)
