d5b20a2bf7b8d729a91c8b5670a48bbc84c6a531
JD Bothma <jbothma@gmail.com>
2016-06-07 17:39:47 +0200

    Only show last four years of audit opinions
    for lack of space

--- sql/table_audit_opinion_facts.sql
+++ sql/table_audit_opinion_facts.sql
@@ -5,7 +5,7 @@
 CREATE TABLE public.audit_report_import
 (
   demarcation_code text NOT NULL,
-  financial_year text NOT NULL,
+  financial_year integer NOT NULL,
   report_url text,
   CONSTRAINT audit_report_import_composit_pkey UNIQUE (demarcation_code, financial_year)
 )
@@ -18,7 +18,7 @@ ALTER TABLE public.audit_report_import
 CREATE TABLE public.audit_opinion_facts
 (
   demarcation_code text NOT NULL,
-  financial_year text NOT NULL,
+  financial_year integer NOT NULL,
   opinion_code text NOT NULL,
   opinion_label text NOT NULL,
   report_url text,
