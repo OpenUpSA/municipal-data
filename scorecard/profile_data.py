089300ec73d979f5b3e15d61e88ba47a2bbcbb1d
JD Bothma <jbothma@gmail.com>
2016-06-23 13:26:48 +0200

    Group income properly and limit to one amount_type

--- scorecard/profile_data.py
+++ scorecard/profile_data.py
@@ -265,25 +265,46 @@ class IndicatorCalculator(object):
         return results
 
     def revenue_breakdown(self):
+        groups = [
+            ('Property rates', ['0200', '0300']),
+            ('Service Charges', ['0400']),
+            ('Rental income', ['0700']),
+            ('Interest and investments', ['0800', '1000', '1100']),
+            ('Fines', ['1300']),
+            ('Licenses and Permits', ['1400']),
+            ('Agency services', ['1500']),
+            ('Transfers for Operating Expenses', ['1600']),
+            ('Transfers for Capital Expenses', ['1610']),
+            ('Other', ['1700', '1800']),
+        ]
         results = {}
         for item in self.results['revenue_breakdown']:
             if item['financial_year_end.year'] not in results:
                 results[item['financial_year_end.year']] = {}
-            results[item['financial_year_end.year']][item['item.code']] = item
+            if item['item.code'] not in results[item['financial_year_end.year']]:
+                results[item['financial_year_end.year']][item['item.code']] = {}
+            results[item['financial_year_end.year']][item['item.code']][item['amount_type.code']] \
+                = item
         values = []
         for year in self.years + [self.budget_year]:
-            year_name = year if year != self.budget_year else ("%s budget" % year)
+            if year == self.budget_year:
+                year_name = "%s budget" % year
+                amount_type = 'ORGB'
+            else:
+                year_name = year
+                amount_type = 'AUDA'
             try:
-                total = results[year]['1900']['amount.sum']
-                for code, item in results[year].iteritems():
-                    if code == '1900':
-                        continue
-                    amount = item['amount.sum']
+                total = results[year]['1900'][amount_type]['amount.sum']
+                for (label, codes) in groups:
+                    amount = 0
+                    for code in codes:
+                        amount += results[year][code][amount_type]['amount.sum'] or 0
                     values.append({
-                        'item': item['item.label'],
+                        'item': label,
                         'amount': amount,
                         'percent': percent(amount, total) if amount else 0,
                         'date': year_name,
+                        'amount_type': amount_type,
                     })
             except KeyError:
                 values.append({
@@ -291,6 +312,7 @@ class IndicatorCalculator(object):
                     'amount': None,
                     'percent': None,
                     'date': year_name,
+                    'amount_type': amount_type,
                 })
 
         return {'values': values}
