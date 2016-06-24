581a84334d2ee76b24881f36be026a464da808df
Greg Kempe <greg@kempe.net>
2016-06-24 14:43:53 +0200

    Quick hack for http basic auth

--- municipal_finance/middleware.py
+++ municipal_finance/middleware.py
@@ -1,4 +1,8 @@
+import base64
+
 from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponse
+from django.conf import settings
 
 from utils import jsonify
 
@@ -19,12 +23,33 @@ class ApiErrorHandler(object):
             }, status=status)
 
 
+def authenticated(request):
+    # Check for valid basic auth header
+    if settings.DEBUG:
+        return True
+
+    if 'HTTP_AUTHORIZATION' in request.META:
+        auth = request.META['HTTP_AUTHORIZATION'].split()
+        if len(auth) == 2:
+            if auth[0].lower() == "basic":
+                uname, passwd = base64.b64decode(auth[1]).split(':')
+                if uname == settings.HTTP_AUTH_USER and passwd == settings.HTTP_AUTH_PASS:
+                    return True
+
+
 class SiteMiddleware(object):
     """ Toggle urls based on site.
     """
     def process_request(self, request):
         site = get_current_site(request)
         if site.name == 'Scorecard':
+            # HACK http-basic auth
+            if not authenticated(request):
+                response = HttpResponse()
+                response.status_code = 401
+                response['WWW-Authenticate'] = 'Basic realm="private"'
+                return response
+
             request.urlconf = 'scorecard.urls'
         else:
             request.urlconf = 'municipal_finance.urls'
