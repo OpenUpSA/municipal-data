10b1b26b76c31e2a8967f23708ca3008dec11220
Greg Kempe <greg@kempe.net>
2016-06-24 11:58:04 +0200

    GA event on video plays

--- scorecard/static/js/scorecard.js
+++ scorecard/static/js/scorecard.js
@@ -82,12 +82,16 @@ $(function() {
     var $btn = $(this);
     $btn.closest('.nav').find('li').removeClass('active');
     $btn.closest('li').addClass('active');
+
     $iframe.attr('src', this.href + "?autoplay=1");
+    ga('send', 'event', 'play-video', $btn.data('lang'));
   });
 
   $('#video-modal')
     .on('show.bs.modal', function() {
-      $iframe.attr('src', $('.video-chooser .video-choices .active a')[0].href + '?autoplay=1');
+      var $target = $('.video-chooser .video-choices .active a');
+      $iframe.attr('src', $target.attr('href') + '?autoplay=1');
+      ga('send', 'event', 'play-video', $target.data('lang'));
     })
     .on('hide.bs.modal', function() {
       $iframe.attr('src', '');
