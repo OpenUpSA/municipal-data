e41ebb7aea21f3f14f07fe0131ffcf5a9229da79
Greg Kempe <greg@kempe.net>
2016-06-24 12:28:25 +0200

    Output xlsx

--- municipal_finance/views.py
+++ municipal_finance/views.py
@@ -2,9 +2,12 @@ from django.http import Http404
 from django.shortcuts import render
 from cubes import get_manager
 
-from utils import jsonify, csvify
+from utils import jsonify, serialize
 
 from django.views.decorators.clickjacking import xframe_options_exempt
+
+
+DUMP_FORMATS = ['csv', 'xlsx']
 
 
 def get_cube(name):
@@ -121,7 +124,7 @@ def aggregate(request, cube_name):
     """ Perform an aggregation request. """
     cube = get_cube(cube_name)
     format = request.GET.get('format', 'json')
-    page_max = request.GET.get('pagesize') if format == 'csv' else 10000
+    page_max = request.GET.get('pagesize') if format in DUMP_FORMATS else 10000
     result = cube.aggregate(aggregates=request.GET.get('aggregates'),
                             drilldowns=request.GET.get('drilldown'),
                             cuts=request.GET.get('cut'),
@@ -132,9 +135,9 @@ def aggregate(request, cube_name):
     if format == 'json':
         result['status'] = 'ok'
         return jsonify(result)
-    elif format == 'csv':
+    elif format in DUMP_FORMATS:
         fields = result['attributes'] + result['aggregates']
-        return csvify(cube_name + '_aggregate', fields, result['cells'])
+        return serialize(format, cube_name + '_aggregate', fields, result['cells'])
 
 
 @xframe_options_exempt
@@ -143,7 +146,7 @@ def facts(request, cube_name):
     materialized dataset. """
     cube = get_cube(cube_name)
     format = request.GET.get('format', 'json')
-    page_max = request.GET.get('pagesize') if format == 'csv' else 10000
+    page_max = request.GET.get('pagesize') if format in DUMP_FORMATS else 10000
     result = cube.facts(fields=request.GET.get('fields'),
                         cuts=request.GET.get('cut'),
                         order=request.GET.get('order'),
@@ -153,8 +156,8 @@ def facts(request, cube_name):
     if format == 'json':
         result['status'] = 'ok'
         return jsonify(result)
-    elif format == 'csv':
-        return csvify(cube_name + '_facts', result['fields'], result['data'])
+    elif format in DUMP_FORMATS:
+        return serialize(format, cube_name + '_facts', result['fields'], result['data'])
 
 
 @xframe_options_exempt
@@ -163,17 +166,18 @@ def members(request, cube_name, member_ref):
     given attribute. """
     cube = get_cube(cube_name)
     format = request.GET.get('format', 'json')
-    page_max = request.GET.get('pagesize') if format == 'csv' else 10000
+    page_max = request.GET.get('pagesize') if format in DUMP_FORMATS else 10000
     result = cube.members(member_ref,
                           cuts=request.GET.get('cut'),
                           order=request.GET.get('order'),
                           page=request.GET.get('page'),
-                          page_size=request.GET.get('pagesize'))
+                          page_size=request.GET.get('pagesize'),
+                          page_max=page_max)
     if format == 'json':
         result['status'] = 'ok'
         return jsonify(result)
-    elif format == 'csv':
-        return csvify(cube_name + '_members', result['fields'], result['data'])
+    elif format in DUMP_FORMATS:
+        return serialize(format, cube_name + '_members', result['fields'], result['data'])
 
 
 @xframe_options_exempt
