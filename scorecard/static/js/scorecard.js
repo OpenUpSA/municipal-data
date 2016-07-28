433cf45ef3b2399763158a2afd58507277faeb54
Greg Kempe <greg@kempe.net>
2016-07-28 11:09:57 +0200

    Social sharing

--- scorecard/static/js/scorecard.js
+++ scorecard/static/js/scorecard.js
@@ -113,3 +113,27 @@ $(function() {
     ga('send', 'event', 'outbound-click', e.target.href);
   });
 });
+
+$(function() {
+  var url = window.location.toString();
+
+  // social buttons
+  $('.fb-share').on('click', function(e) {
+    e.preventDefault();
+
+    window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url),
+                "share", "width=600, height=400, scrollbars=no");
+                ga('send', 'social', 'facebook', 'share', url);
+  });
+
+  $('.twitter-share').on('click', function(e) {
+    e.preventDefault();
+    var tweet = $(this).data('tweet') || '';
+
+    window.open("https://twitter.com/intent/tweet?" +
+                "text=" + encodeURIComponent(tweet) +
+                  "&url=" + encodeURIComponent(url),
+                "share", "width=364, height=250, scrollbars=no");
+                ga('send', 'social', 'twitter', 'share', url);
+  });
+});
