e1693aa35dba5f9a67617ebff620aa2763aa81fe
Greg Kempe <greg@kempe.net>
2016-12-13 11:45:15 +0200

    polyfill for String.startsWith

--- municipal_finance/static/javascript/table.js
+++ municipal_finance/static/javascript/table.js
@@ -1,6 +1,14 @@
 (function(exports) {
   "use strict";
 
+  // polyfill for String.startsWith()
+  if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position) {
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+    };
+  }
+
   // progress indicators
   var spinning = 0;
   function spinnerStart() {
