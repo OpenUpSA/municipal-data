de2bde261f7b321b52e838c570e35812f683b46b
JD Bothma <jbothma@gmail.com>
2017-01-09 13:38:07 +0200

    Update SQL to update contact details

--- sql/upsert/municipalities.sql
+++ sql/upsert/municipalities.sql
@@ -1,5 +1,35 @@
 BEGIN;
 
-\copy scorecard_geography (geo_code, geo_level, parent_level, parent_code, province_code, province_name, category, year, name) FROM '/home/jdb/proj/code4sa/municipal_finance/datasets/2017q1/demarcation-changes/new2016_scorecard_geography.csv' DELIMITER ',' CSV HEADER FORCE NOT NULL name;
+CREATE TEMPORARY TABLE municipality_contacts_upsert
+(
+demarcation_code TEXT,
+postal_address_1 TEXT,
+postal_address_2 TEXT,
+postal_address_3 TEXT,
+street_address_1 TEXT,
+street_address_2 TEXT,
+street_address_3 TEXT,
+street_address_4 TEXT,
+phone_number TEXT,
+fax_number TEXT,
+url TEXT
+) ON COMMIT DROP;
+
+\copy municipality_contacts_upsert (demarcation_code,postal_address_1,postal_address_2,postal_address_3,street_address_1,street_address_2,street_address_3,street_address_4,phone_number,fax_number,url) FROM '/home/jdb/proj/code4sa/municipal_finance/django-app/muni.csv' DELIMITER ',' CSV HEADER;
+
+UPDATE scorecard_geography f
+SET
+postal_address_1 = i.postal_address_1,
+postal_address_2 = i.postal_address_2,
+postal_address_3 = i.postal_address_3,
+street_address_1 = i.street_address_1,
+street_address_2 = i.street_address_2,
+street_address_3 = i.street_address_3,
+street_address_4 = i.street_address_4,
+phone_number = i.phone_number,
+fax_number = i.fax_number,
+url = i.url
+FROM municipality_contacts_upsert i
+WHERE f.geo_code = i.demarcation_code;
 
 COMMIT;
