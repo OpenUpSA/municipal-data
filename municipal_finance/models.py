ac60c9d4c86ba1e9ecde188be1ec8b59f85867a1
JD Bothma <jbothma@gmail.com>
2017-03-01 11:56:39 +0200

    Add strict unique_together constraint

--- municipal_finance/models.py
+++ municipal_finance/models.py
@@ -3,12 +3,9 @@
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 
 from django.db import models
-from scorecard.models import Geography
 
 
 class AgedCreditorFacts(models.Model):
@@ -31,6 +28,17 @@ class AgedCreditorFacts(models.Model):
 
     class Meta:
         db_table = 'aged_creditor_facts'
+        unique_together = (
+            ('demarcation_code', 'period_code', 'item_code'),
+            (
+                'amount_type_code',
+                'demarcation_code',
+                'financial_period',
+                'financial_year',
+                'item_code',
+                'period_length',
+            ),
+        )
 
 
 class AgedCreditorItems(models.Model):
