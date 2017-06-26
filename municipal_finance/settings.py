4595d8fa3081f727060feb8781a2d0b8fc38e73f
JD Bothma <jbothma@gmail.com>
2017-06-26 16:28:55 +0200

    Update dev env
    - ubuntu 14.04 wasn't building any more
    - ubuntu 16.04 comes with modified debian python with ssl3 flag dropped
    - updated genevent to use python > 2.7.8
    - added docker-compose file to make dev env management more consistent

--- municipal_finance/settings.py
+++ municipal_finance/settings.py
@@ -8,6 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+TESTING = False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
