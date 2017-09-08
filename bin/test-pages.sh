4aa78887a2925d8e872c53f6f0ccad9bdf6ee28d
JD Bothma <jbothma@gmail.com>
2017-09-08 16:21:52 +0200

    Update test-pages - not running in docker locally any more

--- bin/test-pages.sh
+++ bin/test-pages.sh
@@ -1,4 +1,4 @@
-for muni in `curl 'http://172.17.0.2:8000/api/cubes/municipalities/members/municipality.demarcation_code?format=csv'|dos2unix|grep -v municipality.demarcation_code`
+for muni in `curl 'http://localhost:8001/api/cubes/municipalities/members/municipality.demarcation_code?format=csv'|dos2unix|grep -v municipality.demarcation_code`
 do
     if [[ ${muni} == DC* ]]
     then
@@ -6,5 +6,5 @@ do
     else
         prefix=municipality-${muni}
     fi
-    curl -o /dev/null -vL http://172.17.0.2:8001/profiles/${prefix}/ 2>&1 | grep HTTP/1 | grep -v 301
+    curl -o /dev/null -vL http://localhost:8000/profiles/${prefix}/ 2>&1 | grep HTTP/1 | grep -v 301
 done
