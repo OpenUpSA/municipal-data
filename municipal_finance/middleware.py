610958b92911456fb7f48e024948754ae97d1df1
lunga <stumenz.complex@gmail.com>
2018-01-09 15:59:23 +0200

    new style middleware format

--- municipal_finance/middleware.py
+++ municipal_finance/middleware.py
@@ -10,6 +10,12 @@ logger = logging.getLogger(__name__)
 
 class ApiErrorHandler(object):
     """ Return API 500-level errors as JSON. """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+    
     def process_exception(self, request, exception):
         if request.path.startswith('/api/'):
             status = getattr(exception, 'http_equiv', 500)
@@ -23,17 +29,32 @@ class ApiErrorHandler(object):
 class SiteMiddleware(object):
     """ Toggle urls based on site.
     """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        self.process_request(request)
+        return self.get_response(request)
+    
     def process_request(self, request):
         site = get_current_site(request)
-
         if site.name == 'Scorecard':
             request.urlconf = 'scorecard.urls'
         else:
             request.urlconf = 'municipal_finance.urls'
 
-
 class RedirectsMiddleware(object):
     """Always redirect www.host to host"""
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.process_request(request)
+        if response is None:
+            return self.get_response(request)
+        else:
+            return response
+    
     def process_request(self, request):
         host = request.get_host()
         if host.startswith("www."):
