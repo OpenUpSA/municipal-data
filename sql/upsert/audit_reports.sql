4d6bf934dad65baaf225adb119b565790d864fa1
JD Bothma <jbothma@gmail.com>
2018-06-04 20:07:36 +0200

    Apply 2018q3 update

--- sql/upsert/audit_reports.sql
+++ sql/upsert/audit_reports.sql
@@ -5,15 +5,16 @@ CREATE TEMPORARY TABLE audit_reports_upsert
         demarcation_code TEXT,
         financial_year integer,
         url TEXT
-) ON COMMIT DROP;
+) ;
 
 \copy audit_reports_upsert (demarcation_code, financial_year, url) FROM '/home/jdb/proj/code4sa/municipalmoney/django-project/audit_reports.csv' DELIMITER ',' CSV HEADER;
+
 
 UPDATE audit_opinion_facts f
 SET report_url = i.url
 FROM audit_reports_upsert i
 WHERE f.demarcation_code = i.demarcation_code
 AND f.financial_year = i.financial_year
-and f.report_url != i.url;
+and (f.report_url <> i.url OR f.report_url is null);
 
 COMMIT;
