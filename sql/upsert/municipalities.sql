16357672ede7fbc7e365f19b0baeaa2746521eab
JD Bothma <jbothma@gmail.com>
2019-07-09 17:44:45 +0200

    Update upserts

--- sql/upsert/municipalities.sql
+++ sql/upsert/municipalities.sql
@@ -15,7 +15,7 @@ fax_number TEXT,
 url TEXT
 ) ON COMMIT DROP;
 
-\copy municipality_contacts_upsert (demarcation_code,postal_address_1,postal_address_2,postal_address_3,street_address_1,street_address_2,street_address_3,street_address_4,phone_number,fax_number,url) FROM '/home/jdb/proj/code4sa/municipalmoney/django-project/muni.csv' DELIMITER ',' CSV HEADER;
+\copy municipality_contacts_upsert (demarcation_code,postal_address_1,postal_address_2,postal_address_3,street_address_1,street_address_2,street_address_3,street_address_4,phone_number,fax_number,url) FROM '/django-project/muni.csv' DELIMITER ',' CSV HEADER;
 
 UPDATE scorecard_geography f
 SET
