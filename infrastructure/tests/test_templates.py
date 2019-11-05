f308aeb82129400a4ab1ba338325bd0e752ed171
Adi Eyal <adi@openup.org.za>
2019-11-05 20:10:54 +0200

    Added all projects view test

--- infrastructure/tests/test_templates.py
+++ infrastructure/tests/test_templates.py
@@ -7,6 +7,9 @@ class TestListPage(TestCase):
     def setUp(cls):
         pass
 
-    def test_assert(self):
+    def test_all_projects_view(self):
         response = self.client.get('/infrastructure/projects')
         self.assertEqual(response.status_code, 200)
+
+        js = response.json()
+        self.assertEquals(len(js["results"]), 3)
