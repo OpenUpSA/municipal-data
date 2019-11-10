e42a88b40aece565db9d6f1bbea8ba6c2ed696b6
Adi Eyal <adi@openup.org.za>
2019-11-10 08:11:52 +0200

    Added project list and detail view

--- infrastructure/urls/templates.py
+++ infrastructure/urls/templates.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from .. import views
 
 urlpatterns = [
-    url(r'^projects$', views.ListView.as_view(), name='project-list-view'),
+    url(r'^projects/$', views.ListView.as_view(), name='project-list-view'),
+    url(r'^projects/(?P<pk>\d+)/$', views.DetailView.as_view(), name='project-detail-view'),
 ]
 
--- infrastructure/views.py
+++ infrastructure/views.py
@@ -1,7 +1,11 @@
+import json
+
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.views.generic.base import TemplateView
+from django.http import JsonResponse
+from django.urls import reverse
 
 from . import models
 from . import serializers
@@ -27,4 +31,34 @@ class ProjectViewSet(viewsets.ReadOnlyModelViewSet):
 
 class ListView(TemplateView):
 
-    template_name = 'infrastructure/list.html'
+    template_name = 'webflow/infrastructure-search.html'
+
+    def get_context_data(self, **kwargs):
+        view = ProjectViewSet.as_view({"get" : "list"}) 
+        api_url = reverse("project-list")
+        self.request.path = api_url
+
+        projects = view(self.request, **kwargs).render().content
+        projects = json.loads(projects)
+
+        projects["view"] = "list";
+        context = super().get_context_data(**kwargs)
+        context['page_data_json'] = {"data" : json.dumps(projects)}
+        return context
+
+class DetailView(TemplateView):
+
+    template_name = 'webflow/infrastructure-project.html'
+
+    def get_context_data(self, **kwargs):
+        view = ProjectViewSet.as_view({"get" : "retrieve"}) 
+        api_url = reverse("project-detail", args=(kwargs["pk"],))
+        self.request.path = api_url
+
+        project = view(self.request, **kwargs).render().content
+        project = json.loads(project)
+
+        print(kwargs)
+        context = super().get_context_data(**kwargs)
+        context['page_data_json'] = {"data" : json.dumps(project)}
+        return context
