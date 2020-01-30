615c42f03fe0504e12fd7ac6d4f2324b1dbc7134
lunga <stumenz.complex@gmail.com>
2020-01-30 13:02:08 +0200

    user can search for municipality

--- infrastructure/static/js/mm-webflow.js
+++ infrastructure/static/js/mm-webflow.js
@@ -736,3 +736,18 @@ function mmWebflow(js) {
 
 }
 
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("muniInput");
+  filter = input.value.toUpperCase();
+  var div = document.getElementById("muniDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    var txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+} 
