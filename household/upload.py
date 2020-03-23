e2d9b45a5ed96400317dd09c3b5057cc38c6b653
ebsuku <stumenz.complex@gmail.com>
2020-03-23 15:35:29 +0200

    check data is unique

--- household/upload.py
+++ household/upload.py
@@ -16,6 +16,7 @@ from .models import (
 )
 from scorecard.models import Geography
 from django.db import transaction
+from django.db import IntegrityError
 
 
 log = logging.getLogger("household.upload")
@@ -55,7 +56,6 @@ def import_bill_data(id):
         raise Exception("csv type unknown")
 
 
-@transaction.atomic
 def household_service_total(csv_obj):
     log.info("Working on service totals")
     csv_file = amazon_s3(csv_obj.csv_file.name)
@@ -71,27 +71,32 @@ def household_service_total(csv_obj):
             household_class = HouseholdClass.objects.get(name=row["Class"])
             service = HouseholdService.objects.get(name=row["Service Name"])
             total = row["Total"] if row["Total"] else None
-            HouseholdServiceTotal.objects.create(
-                geography=geography,
-                financial_year=financial_year,
-                budget_phase=budget_phase,
-                household_class=household_class,
-                version=csv_obj.version,
-                service=service,
-                total=total,
-            )
+            try:
+
+                HouseholdServiceTotal.objects.create(
+                    geography=geography,
+                    financial_year=financial_year,
+                    budget_phase=budget_phase,
+                    household_class=household_class,
+                    version=csv_obj.version,
+                    service=service,
+                    total=total,
+                )
+            except IntegrityError:
+                log.warn(
+                    "Service total for budget phase and financial year already exists"
+                )
     csv_file.close()
     log.info("Completed working on service totals")
 
 
-@transaction.atomic
 def household_bill_total(csv_obj):
-    print("Working on total bill totals")
     log.info("Working on total bill totals")
     csv_file = amazon_s3(csv_obj.csv_file.name)
     csv_file.seek(0)
     with open(csv_file.name, "r") as new_file:
         reader = csv.DictReader(new_file)
+        print("Working on {}")
         for row in reader:
             geography = Geography.objects.get(geo_code=row["Geography"])
             financial_year = FinancialYear.objects.get(
@@ -101,14 +106,20 @@ def household_bill_total(csv_obj):
             household_class = HouseholdClass.objects.get(name=row["Class"])
             percent = row["Percent Increase"] if row["Percent Increase"] else None
             total = row["Total"] if row["Total"] else None
-            HouseholdBillTotal.objects.create(
-                geography=geography,
-                financial_year=financial_year,
-                budget_phase=budget_phase,
-                household_class=household_class,
-                version=csv_obj.version,
-                percent=percent,
-                total=total,
-            )
+            try:
+
+                HouseholdBillTotal.objects.create(
+                    geography=geography,
+                    financial_year=financial_year,
+                    budget_phase=budget_phase,
+                    household_class=household_class,
+                    version=csv_obj.version,
+                    percent=percent,
+                    total=total,
+                )
+            except IntegrityError:
+                log.warn(
+                    "Bill total for budget phase and financial year already exists"
+                )
     csv_file.close()
     log.info("Completed working on bill totals")
