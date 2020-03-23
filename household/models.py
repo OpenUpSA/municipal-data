466892e41d8dd324024185af57237f42354f4002
ebsuku <stumenz.complex@gmail.com>
2020-03-23 13:56:07 +0200

    increase decimal size for incorrect data

--- household/models.py
+++ household/models.py
@@ -11,16 +11,13 @@ class DataSetVersion(models.Model):
         return f"{self.version}"
 
 
-
 class DataSetFile(models.Model):
-    csv_file = models.FileField(upload_to='datasets/')
+    csv_file = models.FileField(upload_to="datasets/")
     version = models.ForeignKey(DataSetVersion, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=20, null=True)
 
     def __str__(self):
-        return f'{self.file_type}-{self.version}'
-
-
+        return f"{self.file_type}-{self.version}"
 
 
 class FinancialYear(models.Model):
@@ -38,7 +35,6 @@ class BudgetPhase(models.Model):
         return self.name
 
 
-
 class HouseholdClass(models.Model):
     name = models.CharField(max_length=100)
 
@@ -53,20 +49,31 @@ class HouseholdService(models.Model):
         return self.name
 
 
-
 class HouseholdServiceTotalQuerySet(models.QuerySet):
     def active(self, geo_code):
-        return self.filter(Q(budget_phase__name='Audited Outcome') | Q(budget_phase__name='Original Budget') | Q(budget_phase__name='Budget Year'),geography__geo_code=geo_code,financial_year__active=True)
+        return self.filter(
+            Q(budget_phase__name="Audited Outcome")
+            | Q(budget_phase__name="Original Budget")
+            | Q(budget_phase__name="Budget Year"),
+            geography__geo_code=geo_code,
+            financial_year__active=True,
+        )
 
     def middle(self):
-        return self.filter(household_class__name='Middle Income Range').values('financial_year__budget_year', 'total', 'service__name')
+        return self.filter(household_class__name="Middle Income Range").values(
+            "financial_year__budget_year", "total", "service__name"
+        )
 
     def affordable(self):
-        return self.filter(household_class__name='Affordable Range').values('financial_year__budget_year', 'total', 'service__name')
+        return self.filter(household_class__name="Affordable Range").values(
+            "financial_year__budget_year", "total", "service__name"
+        )
 
     def indigent(self):
-        return self.filter(household_class__name='Indigent HH receiving FBS').values('financial_year__budget_year', 'total', 'service__name')
-        
+        return self.filter(household_class__name="Indigent HH receiving FBS").values(
+            "financial_year__budget_year", "total", "service__name"
+        )
+
 
 class HouseholdServiceTotal(models.Model):
     geography = models.ForeignKey(Geography, on_delete=models.CASCADE)
@@ -74,49 +81,56 @@ class HouseholdServiceTotal(models.Model):
     budget_phase = models.ForeignKey(BudgetPhase, on_delete=models.CASCADE)
     household_class = models.ForeignKey(HouseholdClass, on_delete=models.CASCADE)
     service = models.ForeignKey(HouseholdService, on_delete=models.CASCADE)
-    total = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    total = models.DecimalField(max_digits=15, decimal_places=2, null=True)
     version = models.ForeignKey(DataSetVersion, on_delete=models.CASCADE, default=1)
 
     objects = models.Manager()
     summary = HouseholdServiceTotalQuerySet.as_manager()
 
     class Meta:
-        unique_together = ('financial_year', 'budget_phase', 'household_class', 'service', 'total')
-
+        unique_together = (
+            "financial_year",
+            "budget_phase",
+            "household_class",
+            "service",
+            "total",
+        )
 
     def __str__(self):
-        return f'{self.household_class} - {self.service} - {self.total}'
+        return f"{self.household_class} - {self.service} - {self.total}"
 
 
 class HouseholdBillTotalQuerySet(models.QuerySet):
     def active(self, geo_code):
         return self.filter(financial_year__active=True, geography__geo_code=geo_code)
-    
+
     def audited(self):
-        return self.filter(
-            budget_phase__name='Audited Outcome').values('financial_year__budget_year',
-                                                         'household_class__name', 'total')
+        return self.filter(budget_phase__name="Audited Outcome").values(
+            "financial_year__budget_year", "household_class__name", "total"
+        )
 
     def original(self):
-        return self.filter(
-            budget_phase__name='Original Budget').values('financial_year__budget_year',
-                                                         'household_class__name', 'total')
+        return self.filter(budget_phase__name="Original Budget").values(
+            "financial_year__budget_year", "household_class__name", "total"
+        )
+
     def budgeted(self):
-        return self.filter(
-            budget_phase__name='Budget Year').values('financial_year__budget_year',
-                                                     'household_class__name', 'total')
-    
+        return self.filter(budget_phase__name="Budget Year").values(
+            "financial_year__budget_year", "household_class__name", "total"
+        )
+
+
 class HouseholdBillTotal(models.Model):
     geography = models.ForeignKey(Geography, on_delete=models.CASCADE)
     financial_year = models.ForeignKey(FinancialYear, on_delete=models.CASCADE)
     budget_phase = models.ForeignKey(BudgetPhase, on_delete=models.CASCADE)
     household_class = models.ForeignKey(HouseholdClass, on_delete=models.CASCADE)
     percent = models.FloatField(null=True)
-    total = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    total = models.DecimalField(max_digits=15, decimal_places=2, null=True)
     version = models.ForeignKey(DataSetVersion, on_delete=models.CASCADE, default=1)
 
     objects = models.Manager()
     summary = HouseholdBillTotalQuerySet.as_manager()
 
     def __str__(self):
-        return f'{self.household_class} - {self.total}'
+        return f"{self.household_class} - {self.total}"
