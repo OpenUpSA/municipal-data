23c9c1decf90aece7dd45d5064301c90ae9442fa
ebsuku <stumenz.complex@gmail.com>
2020-05-15 10:20:20 +0200

    count number of achived for each category

--- metro/utils.py
+++ metro/utils.py
@@ -1,6 +1,6 @@
 def category_sort(queryset):
     """
-    Sort the quarterly results in to categories
+    Sort the quarterly results into categories
     """
     category = {}
     if queryset:
@@ -8,8 +8,11 @@ def category_sort(queryset):
             category[result.indicator.category.name] = {
                 "slug": result.indicator.category.slug,
                 "results": [],
+                "count": 0,
             }
 
         for result in queryset:
             category[result.indicator.category.name]["results"].append(result)
+            if result.target_achived():
+                category[result.indicator.category.name]["count"] += 1
     return category
