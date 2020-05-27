26dc4d47b0edb2e8111e6e74164982aa9dfd8090
ebsuku <stumenz.complex@gmail.com>
2020-05-27 14:37:45 +0200

    add infrastructure to admin

--- infrastructure/admin.py
+++ infrastructure/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+
+@admin.register(models.FinancialYear)
+class FinancialYearAdmin(admin.ModelAdmin):
+    list_display = ("budget_year",)
+
+
+@admin.register(models.BudgetPhase)
+class BudgetPhaseAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "project_number",
+        "project_type",
+        "function",
+        "ward_location",
+        "latitude",
+        "longitude",
+    )
+
+
+@admin.register(models.Expenditure)
+class ExpenditureAdmin(admin.ModelAdmin):
+    list_display = (
+        "project",
+        "budget_phase",
+        "financial_year",
+        "amount",
+    )
