d3528bbe1fec0d781c265d34dd77747cb832e77b
ebsuku <stumenz.complex@gmail.com>
2020-06-24 17:26:17 +0200

    setup aws for prod

--- municipal_finance/settings.py
+++ municipal_finance/settings.py
@@ -363,14 +363,14 @@ Q_CLUSTER = {
     "orm": "default",
 }
 
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
-AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    "CacheControl": "max-age=86400",
-}
-
 if not DEBUG:
     DEFAULT_FILE_STORAGE = "municipal_finance.storage.MediaStorage"
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get(
+        "AWS_STORAGE_BUCKET_NAME", "munimoney-media"
+    )
+    AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+        "CacheControl": "max-age=86400",
+    }
