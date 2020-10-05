3b703eeee3bf583fb9750a8d6bd5e899d990bb4f
Jurie-Jan Botha <juriejanbotha@gmail.com>
2020-10-05 17:09:49 +0200

    Added a test for updating the existing contacts

--- municipal_finance/tests/test_update.py
+++ municipal_finance/tests/test_update.py
@@ -11,18 +11,28 @@ class UpdateMunicipalContactsTestCase(TestCase):
         self.user = User.objects.create_user(
             username="sample", email="sample@some.co", password="testpass",
         )
-        self.upload = MunicipalityStaffContactsUpload.objects.create(
+        self.initial_upload = MunicipalityStaffContactsUpload.objects.create(
             user=self.user,
             file=File(
                 open(
-                    'municipal_finance/fixtures/municipal_contacts.csv',
+                    'municipal_finance/fixtures/municipal_contacts/initial.csv',
+                    'rb',
+                ),
+            ),
+        )
+        self.update_upload = MunicipalityStaffContactsUpload.objects.create(
+            user=self.user,
+            file=File(
+                open(
+                    'municipal_finance/fixtures/municipal_contacts/update.csv',
                     'rb',
                 ),
             ),
         )
 
-    def test_upload_processes_correctly(self):
-        result = update_municipal_staff_contacts(self.upload)
+    def test_initial_and_update(self):
+        # Upload the initial data
+        result = update_municipal_staff_contacts(self.initial_upload)
         records = MunicipalityStaffContacts.objects.all()
         self.assertEquals(result, {"updated": 0, "created": 4})
         self.assertQuerysetEqual(records, [
@@ -61,5 +71,56 @@ class UpdateMunicipalContactsTestCase(TestCase):
                 office_number="012 123 4444",
                 fax_number="081 123 4444",
                 email_address="four@some.co",
+            ),
+        ], transform=lambda x: x, ordered=False)
+        # Upload an update
+        result = update_municipal_staff_contacts(self.update_upload)
+        records = MunicipalityStaffContacts.objects.all()
+        self.assertEquals(result, {"updated": 2, "created": 1})
+        self.assertQuerysetEqual(records, [
+            MunicipalityStaffContacts(
+                demarcation_code="AAA",
+                role="Role A",
+                title="Mr",
+                name="New First Name",
+                office_number="012 321 1111",
+                fax_number="081 321 1111",
+                email_address="one@updated.co",
+            ),
+            MunicipalityStaffContacts(
+                demarcation_code="AAA",
+                role="Role B",
+                title="Mr",
+                name="Second Name",
+                office_number="012 123 2222",
+                fax_number="081 123 2222",
+                email_address="two@some.co",
+            ),
+            MunicipalityStaffContacts(
+                demarcation_code="BBB",
+                role="Role A",
+                title="Dr",
+                name="Third Name",
+                office_number="012 123 3333",
+                fax_number="081 123 3333",
+                email_address="three@some.co",
+            ),
+            MunicipalityStaffContacts(
+                demarcation_code="BBB",
+                role="Role B",
+                title="Mr",
+                name="Fourth Name Updated",
+                office_number="012 321 4444",
+                fax_number="081 321 4444",
+                email_address="four@updated.co",
+            ),
+            MunicipalityStaffContacts(
+                demarcation_code="CCC",
+                role="Role A",
+                title="Mr",
+                name="Five Name",
+                office_number="012 123 5555",
+                fax_number="081 123 5555",
+                email_address="five@some.co",
             ),
         ], transform=lambda x: x, ordered=False)
