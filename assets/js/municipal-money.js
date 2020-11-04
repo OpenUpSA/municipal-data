63baa9b090f1fb2a6fbd2970e26d2fef83639ad8
JD Bothma <jbothma@gmail.com>
2020-11-04 18:21:35 +0200

    Template in muni name

--- assets/js/municipal-money.js
+++ assets/js/municipal-money.js
@@ -1,1 +1,19 @@
-const a = "b";
+function logIfUnequal(a, b) {
+  if (a !== b) {
+    console.error(`${a} !== ${b}`);
+  }
+}
+
+class Title {
+  getSelector = () => ".page-heading__title";
+
+  constructor(title) {
+    this.element = $(this.getSelector());
+    logIfUnequal(1, this.element.length);
+    this.element.text(title);
+  }
+}
+
+const pageData = JSON.parse(document.getElementById('page-data').textContent);
+
+new Title(pageData.geography.name);
