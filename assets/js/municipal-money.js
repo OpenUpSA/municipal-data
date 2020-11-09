b2a9b19205257b54a5e91dc12fb798da5c616ddc
JD Bothma <jbothma@gmail.com>
2020-11-09 20:30:23 +0200

    Add number formatting and remaining demographic indicators

--- assets/js/municipal-money.js
+++ assets/js/municipal-money.js
@@ -1,16 +1,15 @@
+import { formatLocale } from 'd3-format';
+
+var D3_LOCALE = formatLocale({
+  decimal: ".",
+  thousands: " ",
+  grouping: [3],
+  currency: ["R", ""],
+});
+
 function logIfUnequal(a, b) {
   if (a !== b) {
     console.error(`${a} !== ${b}`);
-  }
-}
-
-class Title {
-  selector = ".page-heading__title";
-
-  constructor(title) {
-    this.element = $(this.selector);
-    logIfUnequal(1, this.element.length);
-    this.element.text(title);
   }
 }
 
@@ -21,7 +20,10 @@ class TextField {
     this.element.text(value);
   }
 }
+
 const pageData = JSON.parse(document.getElementById('page-data').textContent);
 
-new Title(pageData.geography.short_name);
-new TextField(".profile-metric__population", pageData.total_population);
+new TextField(".page-heading__title", pageData.geography.short_name);
+new TextField(".profile-metric__population", D3_LOCALE.format(",")(pageData.total_population));
+new TextField(".profile-metric__size", D3_LOCALE.format(",.1f")(pageData.geography.square_kms));
+new TextField(".profile-metric__density", D3_LOCALE.format(",.1f")(pageData.population_density));
