5ab866ace60c7fe24b8cc3ff005498e7befc0473
JD Bothma <jbothma@gmail.com>
2020-11-21 18:29:17 +0200

    Fill in the remaining contact details

--- assets/js/components/contacts.js
+++ assets/js/components/contacts.js
@@ -5,9 +5,34 @@ class OfficeContact {
 
   constructor(office) {
     this.$element = $(this.templateSelector).clone();
-    this.$element.find(".expand-block__heading").text(office.role);
-    this.$element.find(".profile-info__item:eq(0) > div:eq(1)").text(office.name);
-    this.$element.find(".profile-info__heading").text(office.secretary.role);
+    this.$itemTemplate = this.$element.find(".profile-info__item ").clone();
+    this.$element.find(".profile-info__item ").remove();
+
+    const $heading = this.$element.find(".expand-block__heading");
+    $heading.text(office.role);
+    const $innerHeading = this.$element.find(".profile-info__heading");
+    $innerHeading.text(office.secretary.role);
+
+    this.makeItem("fas fa-user-alt", office.name).insertBefore($innerHeading);
+    this.makeItem("fas fa-phone", office.office_phone, "tel").insertBefore($innerHeading);
+    this.makeItem("fas fa-envelope", office.email, "mailto").insertBefore($innerHeading);
+    this.makeItem("fas fa-envelope", office.secretary.email, "mailto").insertAfter($innerHeading);
+    this.makeItem("fas fa-phone", office.secretary.office_phone, "tel").insertAfter($innerHeading);
+    this.makeItem("fas fa-user-alt", office.secretary.name).insertAfter($innerHeading);
+
+  }
+
+  makeItem(iconClasses, value, linkType) {
+    const $item = this.$itemTemplate.clone();
+    $item.find(".profile-item__item_icon > div").attr("class", iconClasses);
+    if (linkType) {
+      const anchor = $("<a></a>").attr("href", `${linkType}:${value}`);
+      anchor.text(value);
+      $item.find(".profile-item__item-value").html(anchor);
+    } else {
+      $item.find(".profile-item__item-value").text(value);
+    }
+    return $item;
   }
 
   render() {
