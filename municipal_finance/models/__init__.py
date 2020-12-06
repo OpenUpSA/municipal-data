66bdd15263df16fc072690321a091578ec622dba
Jurie-Jan Botha <juriejanbotha@gmail.com>
2020-12-06 15:59:07 +0200

    Models, cubes and import script for v2 Aged Credit data

--- municipal_finance/models/__init__.py
+++ municipal_finance/models/__init__.py
@@ -63,51 +63,12 @@ from .aged_debtor import (
     AgedDebtorItemsV2,
     AgedDebtorFactsV2,
 )
-
-
-class AgedCreditorFacts(models.Model):
-    demarcation_code = models.TextField()
-    period_code = models.TextField()
-    item_code = models.ForeignKey(
-        'AgedCreditorItems', models.DO_NOTHING, db_column='item_code')
-    g1_amount = models.BigIntegerField(null=True)
-    l1_amount = models.BigIntegerField(null=True)
-    l120_amount = models.BigIntegerField(null=True)
-    l150_amount = models.BigIntegerField(null=True)
-    l180_amount = models.BigIntegerField(null=True)
-    l30_amount = models.BigIntegerField(null=True)
-    l60_amount = models.BigIntegerField(null=True)
-    l90_amount = models.BigIntegerField(null=True)
-    total_amount = models.BigIntegerField(null=True)
-    financial_year = models.IntegerField()
-    period_length = models.TextField()
-    financial_period = models.IntegerField()
-    amount_type_code = models.TextField()
-
-    class Meta:
-        db_table = 'aged_creditor_facts'
-        unique_together = (
-            ('demarcation_code', 'period_code', 'item_code'),
-            (
-                'amount_type_code',
-                'demarcation_code',
-                'financial_period',
-                'financial_year',
-                'item_code',
-                'period_length',
-            ),
-        )
-
-
-class AgedCreditorItems(models.Model):
-    code = models.TextField(primary_key=True)
-    label = models.TextField()
-    position_in_return_form = models.IntegerField(null=True)
-    return_form_structure = models.TextField(null=True)
-    composition = models.TextField(null=True)
-
-    class Meta:
-        db_table = 'aged_creditor_items'
+from .aged_creditor import (
+    AgedCreditorItemsV1,
+    AgedCreditorFactsV1,
+    AgedCreditorItemsV2,
+    AgedCreditorFactsV2,
+)
 
 
 class AuditOpinionFacts(models.Model):
