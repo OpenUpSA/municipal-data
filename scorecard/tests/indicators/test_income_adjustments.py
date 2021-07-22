d95e8939ea432213ded79e0c30c2756bfd6170ed
JD Bothma <jbothma@gmail.com>
2021-07-22 17:58:17 +0200

    Add test showing v2 data replacing v1 data

--- scorecard/tests/indicators/test_income_adjustments.py
+++ scorecard/tests/indicators/test_income_adjustments.py
@@ -143,3 +143,73 @@ class RevenueSourcesTests(SimpleTestCase):
         }
         actual = IncomeAdjustments.get_muni_specifics(api_data)
         self.assertEqual(expected, actual)
+
+    def test_v2(self):
+        """
+        - v2 data is calculated correctly
+        - v2 results replace v1 results even if it's for different item labels
+            (i.e. replacement is for the entire set, not per-label)
+        """
+        api_data = MockAPIData(
+            {
+                "revenue_budget_actual_v1": [
+                    {
+                        "item.code": "1300",
+                        "amount.sum": 200,
+                        "financial_year_end.year": 2050,
+                        "amount_type.code": "ORGB",
+                    },
+                    {
+                        "item.code": "1300",
+                        "amount.sum": 210,
+                        "financial_year_end.year": 2050,
+                        "amount_type.code": "ADJB",
+                    },
+                    {
+                        "item.code": "1300",
+                        "amount.sum": 220,
+                        "financial_year_end.year": 2050,
+                        "amount_type.code": "AUDA",
+                    },
+                ],
+                "revenue_budget_actual_v2": [
+                    {
+                        "item.code": "0200",
+                        "amount.sum": 300,
+                        "financial_year_end.year": 2050,
+                        "amount_type.code": "ORGB",
+                    },
+                    {
+                        "item.code": "0200",
+                        "amount.sum": 310,
+                        "financial_year_end.year": 2050,
+                        "amount_type.code": "ADJB",
+                    },
+                    {
+                        "item.code": "0200",
+                        "amount.sum": 320,
+                        "financial_year_end.year": 2050,
+                        "amount_type.code": "AUDA",
+                    },
+                ],
+            },
+            [2050, 2049, 2048, 2047]
+        )
+        expected = {
+            2050: [
+                {
+                    "item": "Property rates",
+                    "amount": 10,
+                    "comparison": "Original to adjusted budget",
+                    "percent_changed": 3.33
+                },
+                {
+                    "item": "Property rates",
+                    "amount": 20,
+                    "comparison": "Original budget to audited outcome",
+                    "percent_changed": 6.67
+                },
+            ]
+        }
+        actual = IncomeAdjustments.get_muni_specifics(api_data)
+        self.assertEqual(expected, actual)
