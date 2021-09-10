19a96ef3a6a66df1b3f85fb6c27635180a68102b
JD Bothma <jbothma@gmail.com>
2021-09-10 13:18:03 +0200

    Run the task explicitly

--- infrastructure/tests/test_data_import.py
+++ infrastructure/tests/test_data_import.py
@@ -5,21 +5,19 @@ from io import BytesIO
 from django.contrib.auth.models import User
 from django.contrib.admin.sites import AdminSite
 from django_q.models import OrmQ
-from django_q.tasks import AsyncTask, fetch, result
-import time
 import rest_framework.response
 
 from infrastructure.models import FinancialYear, QuarterlySpendFile, AnnualSpendFile, Expenditure, Project
 from infrastructure.tests import utils
 from infrastructure.utils import load_excel
+from infrastructure.upload import process_document
 from scorecard.models import Geography
 
 
-@override_settings(Q_CLUSTER={**settings.Q_CLUSTER, 'sync': True})
 class FileTest(TransactionTestCase):
+    fixtures = ["seeddata"]
 
     def setUp(self):
-        fixtures = ["test_infrastructure.json"]
         self.client = Client()
         self.username = 'admin'
         self.email = 'test@whatever.com'
@@ -38,26 +36,31 @@ class FileTest(TransactionTestCase):
         self.client.login(username=self.username, password=self.password)
         fy = FinancialYear.objects.create(budget_year="2019/2020", active=1)
 
-        self.assertEquals(FinancialYear.objects.all().count(), 1)
         self.assertEquals(AnnualSpendFile.objects.all().count(), 0)
+        self.assertEqual(OrmQ.objects.count(), 0)
 
         # the app name, the name of the model and the name of the view
-        url = reverse('admin:infrastructure_annualspendfile_add')
+        upload_url = reverse('admin:infrastructure_annualspendfile_add')
 
         with open('infrastructure/tests/test_files/test.xlsx', 'rb', ) as f:
-            resp = self.client.post(url, {'financial_year': fy.pk, 'document': f}, follow=True)
+            resp = self.client.post(upload_url, {'financial_year': fy.pk, 'document': f}, follow=True)
         self.assertContains(resp, "Dataset is currently being processed.", status_code=200)
 
-        file = AnnualSpendFile.objects.all().values("id")
+        spend_file = AnnualSpendFile.objects.first()
+        self.assertEquals(spend_file.status, AnnualSpendFile.PROGRESS)
 
-        a = AsyncTask('infrastructure.upload.process_document', file[0]['id'], sync=True)
-        a.run()
+        self.assertEqual(OrmQ.objects.count(), 1)
+        task = OrmQ.objects.first()
+        task_file_id = task.task()["args"][0]
+        task_method = task.func()
+        self.assertEqual(task_method, 'infrastructure.upload.process_document')
+        self.assertEqual(task_file_id, spend_file.id)
+        # run the code
+        process_document(task_file_id)
 
-        fileSpend = AnnualSpendFile.objects.all().values("status")
-        filestatus = fileSpend[0]['status']
-
-        self.assertEquals(AnnualSpendFile.objects.all().count(), 1)
-        self.assertEquals(filestatus, AnnualSpendFile.SUCCESS)
+        self.assertEquals(AnnualSpendFile.objects.count(), 1)
+        spend_file = AnnualSpendFile.objects.first()
+        self.assertEquals(spend_file.status, AnnualSpendFile.SUCCESS)
         self.assertEquals(Project.objects.count(), 2)
 
         response = self.client.get("/api/v1/infrastructure/search/")
