948d6e623b8a685068e602cb4526c154d97d9f5f
michaelglenister <mickeyjhg@gmail.com>
2021-09-22 14:06:30 +0200

    Make quarterly uploads work
    Adding seperate methods to handle the quarterly uploads, using the
    QuaryerlyFile model

--- infrastructure/upload.py
+++ infrastructure/upload.py
@@ -4,9 +4,9 @@ from .utils import load_excel
 import io
 
 
-def process_document(id):
+def process_annual_document(id):
     """
-    Get file an process it.
+    Get file and process it.
     """
     spend = AnnualSpendFile.objects.get(id=id)
 
@@ -18,4 +18,20 @@ def process_document(id):
     except Exception:
         spend.status = AnnualSpendFile.ERROR
         spend.save()
-        raise ValueError("Error processing file")
+        raise ValueError("Error processing annual spend file")
+
+def process_quarterly_document(id):
+    """
+    Get file and process it.
+    """
+    spend = QuarterlySpendFile.objects.get(id=id)
+
+    try:
+        file_contents = spend.document.read()
+        load_excel("", financial_year=spend.financial_year, file_contents=file_contents)
+        spend.status = QuarterlySpendFile.SUCCESS
+        spend.save()
+    except Exception:
+        spend.status = QuarterlySpendFile.ERROR
+        spend.save()
+        raise ValueError("Error processing quarterly spend file")
