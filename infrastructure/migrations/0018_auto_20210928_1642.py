caf18465d0850a0b616d64c7c2a70d8e6bb5b965
michaelglenister <mickeyjhg@gmail.com>
2021-10-06 14:59:24 +0200

    Move RunPython into a seperate migration

--- infrastructure/migrations/0018_auto_20210928_1642.py
+++ infrastructure/migrations/0018_auto_20210928_1642.py
@@ -6,6 +6,27 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
+def populate_implementation_year(apps, schema_editor):
+    project = apps.get_model("infrastructure", "Project")
+    year = apps.get_model("infrastructure", "FinancialYear")
+
+    db_alias = schema_editor.connection.alias
+
+    try:
+        implementation_year_id = year.objects.using(db_alias).get(budget_year="2019/2020")
+    except ObjectDoesNotExist:
+        implementation_year_id = 1
+
+    for proj in project.objects.using(db_alias).all():
+        proj.latest_implementation_year = implementation_year_id
+        proj.save()
+
+def reverse_implementation_year(apps, schema_editor):
+    migrations.RemoveField(
+        model_name='project',
+        name='latest_implementation_year',
+    ),
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -13,9 +34,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='project',
-            name='latest_implementation_year',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='infrastructure.FinancialYear'),
-        ),
+        migrations.RunPython(populate_implementation_year, reverse_implementation_year),
     ]
