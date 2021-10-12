5a1a11230bf951577b9f369b04ed0925e864a8fb
michaelglenister <mickeyjhg@gmail.com>
2021-10-12 15:31:39 +0200

    Use an arbitrary year to test the budget year set

--- infrastructure/tests/test_project_details.py
+++ infrastructure/tests/test_project_details.py
@@ -22,7 +22,7 @@ class CapitalProjectTest(BaseSeleniumTestCase):
             category="A",
         )
 
-        self.fy = FinancialYear.objects.create(budget_year="2019/2020", active=1)
+        self.fy = FinancialYear.objects.create(budget_year="2049/2050", active=1)
 
         fields = {
             "geography": self.geography,
@@ -41,19 +41,16 @@ class CapitalProjectTest(BaseSeleniumTestCase):
         }
         self.project = Project.objects.create(**fields)
 
-        self.create_expenditure(15500000, "Full Year Forecast", "2018/2019")
-        self.create_expenditure(5500000, "Budget year", "2019/2020")
-        self.create_expenditure(6000000, "Budget year", "2020/2021")
-        financial_year = FinancialYear.objects.create(budget_year="2021/2022")
+        self.create_expenditure(15500000, "Full Year Forecast", "2048/2049")
+        self.create_expenditure(5500000, "Budget year", "2049/2050")
+        self.create_expenditure(6000000, "Budget year", "2050/2051")
+        financial_year = FinancialYear.objects.create(budget_year="2051/2052")
 
         super(CapitalProjectTest, self).setUp()
 
     def test_project_details(self):
         selenium = self.selenium
-        selenium.get("%s%s" % (self.live_server_url, "/infrastructure/projects/"))
-        self.wait_until_text_in(".page-heading", "Municipal Infrastructure Projects")
-
-        selenium.find_element_by_css_selector(".narrow-card_first-column-2").click()
+        selenium.get("%s%s" % (self.live_server_url, "/infrastructure/projects/1/"))
 
         self.wait_until_text_in(".project-description", "P-CNIEU COM FAC HALLS")
         self.wait_until_text_in(".project-number__value", "PC002002002002001001_00001")
@@ -64,13 +61,13 @@ class CapitalProjectTest(BaseSeleniumTestCase):
         self.wait_until_text_in(".iudf", "Inclusion and access")
         self.wait_until_text_in(".province", "Eastern Cape")
         self.wait_until_text_in(".ward", "Coastal,Whole of the Metro,...")
-        self.wait_until_text_in(".full-year-forecast .year", "2018/2019")
+        self.wait_until_text_in(".full-year-forecast .year", "2048/2049")
         self.wait_until_text_in(".project-detail_text.forecast", "R15.50 Million")
-        self.wait_until_text_in(".budget-year-1 .year", "2019/2020")
+        self.wait_until_text_in(".budget-year-1 .year", "2049/2050")
         self.wait_until_text_in(".project-detail_text.budget1", "R5.50 Million")
-        self.wait_until_text_in(".budget-year-2 .year", "2020/2021")
+        self.wait_until_text_in(".budget-year-2 .year", "2050/2051")
         self.wait_until_text_in(".project-detail_text.budget2", "R6.00 Million")
-        self.wait_until_text_in(".budget-year-3 .year", "2021/2022")
+        self.wait_until_text_in(".budget-year-3 .year", "2051/2052")
         self.wait_until_text_in(".project-detail_text.budget3", "Not available")
 
         self.wait_until_text_in(".subsection-chart_wrapper .project-detail_heading", "NO DATA AVAILABLE")
@@ -83,7 +80,6 @@ class CapitalProjectTest(BaseSeleniumTestCase):
         )
 
         selenium = self.selenium
-        selenium.get("%s%s" % (self.live_server_url, "/infrastructure/projects/"))
-        selenium.find_element_by_css_selector(".narrow-card_first-column-2").click()
+        selenium.get("%s%s" % (self.live_server_url, "/infrastructure/projects/2/"))
 
-        self.wait_until_text_in(".subsection-chart_wrapper", "Financial Year 2019/2020")
+        self.wait_until_text_in(".subsection-chart_wrapper", "Financial Year 2049/2050")
