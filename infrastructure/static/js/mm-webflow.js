9b89d2bd2eea1b9d9b3b1a841714971fda80aa91
michaelglenister <mickeyjhg@gmail.com>
2021-11-02 17:30:20 +0200

    Display warning when viewing projects not up to date

--- infrastructure/static/js/mm-webflow.js
+++ infrastructure/static/js/mm-webflow.js
@@ -848,6 +848,11 @@ function mmWebflow(js) {
 
         $(".detail-button_wrapper").hide();
         $(".subsection-chart__detail").hide();
+
+        var summaryYear = js.summary_year
+        if (summaryYear.split('/')[0] > implementYear.split('/')[0]) {
+            $(".project-details__info-message").parent().append('This project was last updated with ' + implementYear + ' data. Please see the search page for the latest figures.');
+        }
     }
 
     if (js["view"] == "list")
