eee1f48d912e58dba23898e9af0ba2388a4ecedc
michaelglenister <mickeyjhg@gmail.com>
2021-11-22 20:18:30 +0200

    Add test for search results

--- infrastructure/tests/test_search_page.py
+++ infrastructure/tests/test_search_page.py
@@ -1,21 +1,15 @@
-from django.test import Client
-from django.urls import reverse
-from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
-from django_q.models import OrmQ
+from django.core.management import call_command
 
-from infrastructure.models import FinancialYear, Project, AnnualSpendFile, ProjectQuarterlySpend, Expenditure, BudgetPhase
-from infrastructure.upload import process_annual_document
+from infrastructure.models import FinancialYear
 from scorecard.models import Geography
-
 from infrastructure.tests.helpers import BaseSeleniumTestCase
 from infrastructure.tests import utils
 
 import urllib.request
 
 
-def mock_project_row():
-    """This is the project data that mock_existing_project_row will update"""
+def mock_project_one():
     mock_data = { 'Function': 'Administrative and Corporate Support',
         'Project Description': 'P-CNIN FURN & OFF EQUIP',
         'Project Number': 'PC002003005_00002',
@@ -35,60 +29,87 @@ def mock_project_row():
     }
     yield mock_data
 
+
+def mock_project_two():
+    mock_data = { 'Function': 'Economic Development/Planning',
+        'Project Description': 'P-CIN RDS ROADS',
+        'Project Number': 'PC001002006001_00028',
+        'Type': 'New',
+        'MTSF Service Outcome': 'An efficient, competitive and responsive economic infrastructure network',
+        'IUDF': 'Growth',
+        'Own Strategic Objectives': 'OWN MUNICIPAL STRATEGIC OBJECTIVE',
+        'Asset Class': 'Roads Infrastructure',
+        'Asset Sub-Class': '',
+        'Ward Location': 'Coastal,Midland,...',
+        'GPS Longitude': '0', 'GPS Latitude': '0',
+        'Audited Outcome 2017/18': 7000.0,
+        'Full Year Forecast 2018/19': 8000.0,
+        'Budget year 2019/20': 9000.0,
+        'Budget year 2020/21': 10000.0,
+        'Budget year 2021/22': 11000.0
+    }
+    yield mock_data
+
 def download_column_headers():
     columns = "province,municipality,project_number,project_description,project_type,function,asset_class,mtsf_service_outcome,own_strategic_objectives,iudf,budget phase,financial year,amount,latitude,longitude"
     return columns
 
 def project_download_data():
-    project_data = 'Eastern Cape,,PC002003005_00002,P-CNIN FURN & OFF EQUIP,New,Administrative and Corporate Support,Furniture and Office Equipment,"An efficient, effective and development-oriented public service",OWN MUNICIPAL STRATEGIC OBJECTIVE,Growth,Budget year,2019/2020,4000.00,0.0,0.0'
+    project_data = 'Eastern Cape,Buffalo City,PC002003005_00002,P-CNIN FURN & OFF EQUIP,New,Administrative and Corporate Support,Furniture and Office Equipment,"An efficient, effective and development-oriented public service",OWN MUNICIPAL STRATEGIC OBJECTIVE,Growth,Budget year,2019/2020,4000.00,0.0,0.0'
     return project_data
+
+def get_download_row(row_contents):
+    row = row_contents.decode('utf-8')
+    row = row.strip('\n')
+    row = row.strip('\r')
+    return row
 
 
 class CapitalSearchTest(BaseSeleniumTestCase):
-    fixtures = ["seeddata"]
+    fixtures = ["demo-data", "seeddata"]
 
     def setUp(self):
-        self.geography = Geography.objects.create(
-            geo_level="municipality",
-            geo_code="BUF",
-            province_name="Eastern Cape",
-            province_code="EC",
-            category="A",
-        )
-
         FinancialYear.objects.create(budget_year="2019/2020")
 
         super(CapitalSearchTest, self).setUp()
         Site.objects.filter(id=2).update(domain='municipalmoney.org.za', name='Scorecard')
-        
+
     def test_search_results(self):
+        call_command('collectstatic', interactive=False)
+
+        geography = Geography.objects.get(geo_code="BUF")
+        utils.load_file(geography, mock_project_one(), "2019/2020")
+        geography = Geography.objects.get(geo_code="TSH")
+        utils.load_file(geography, mock_project_two(), "2019/2020")
+
+        selenium = self.selenium
+        selenium.get("%s%s" % (self.live_server_url, "/infrastructure/projects/?municipality=Buffalo+City"))
+
+        self.wait_until_text_in(".search-detail_projects", "1")
+
+        self.wait_until_text_in("#result-list-container", "P-CNIN FURN & OFF EQUIP")
+        self.wait_until_text_in("#result-list-container", "ADMINISTRATIVE AND CORPORATE SUPPORT")
+        self.wait_until_text_in("#result-list-container", "R4.00 K")
+
+    def download_button_exists(self):
         selenium = self.selenium
         selenium.get("%s%s" % (self.live_server_url, "/infrastructure/projects/"))
-        
-        utils.load_file(self.geography, mock_project_row(), "2019/2020")
-        
-        project = Project.objects.get(project_description="P-CNIN FURN & OFF EQUIP")
-        self.assertEquals(project.project_description, "P-CNIN FURN & OFF EQUIP")
-        #self.wait_until_text_in("#result-list-container", "P-CNIN FURN & OFF EQUIP")
-        
-    def test_download_button_exists(self):
-        selenium = self.selenium
-        selenium.get("%s%s" % (self.live_server_url, "/infrastructure/projects/"))
-        self.wait_until_text_in("#Search-Button", "Search")
-    
+
+        download_button = selenium.find_element_by_id('Download-Button')
+        download_button.click()
+        self.assertEquals(download_button.get_attribute("href"), f"{self.live_server_url}/infrastructure/download?budget_phase=Budget+year&financial_year=2019%2F2020")
+
     def test_download_url(self):
-        utils.load_file(self.geography, mock_project_row(), "2019/2020")
+        geography = Geography.objects.get(geo_code="BUF")
+        utils.load_file(geography, mock_project_one(), "2019/2020")
 
         url = f"{self.live_server_url}/infrastructure/download?budget_phase=Budget+year&financial_year=2019%2F2020"
         response = urllib.request.urlopen(url)
         contents = response.readlines()
 
-        headers = contents[0].decode('utf-8')
-        headers = headers.strip('\n')
-        headers = headers.strip('\r')
-        column_data = contents[1].decode('utf-8')
-        column_data = column_data.strip('\n')
-        column_data = column_data.strip('\r')
+        headers = get_download_row(contents[0])
+        project_row = get_download_row(contents[1])
 
+        self.assertEquals(len(contents), 2)
         self.assertEquals(download_column_headers(), headers)
-        self.assertEquals(project_download_data(), column_data)
+        self.assertEquals(project_download_data(), project_row)
