a7ec3103725c20186973028a7a49399f8afcfc99
michaelglenister <mickeyjhg@gmail.com>
2021-12-07 16:44:30 +0200

    Add GA to infra search and details pages

--- infrastructure/static/js/mm-webflow.js
+++ infrastructure/static/js/mm-webflow.js
@@ -708,6 +708,7 @@ function mmWebflow(js) {
         });
 
         triggerSearch();
+        add_google_analytics();
     }
 
     function mmDetailView(js) {
@@ -848,6 +849,7 @@ function mmWebflow(js) {
 
         $(".detail-button_wrapper").hide();
         $(".subsection-chart__detail").hide();
+        add_google_analytics();
     }
 
     if (js["view"] == "list")
@@ -873,4 +875,16 @@ function filterFunction() {
       a[i].style.display = "none";
     }
   }
-} 
+}
+
+function add_google_analytics() {
+    addScriptToBody(window, {}, "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\
+        {% if GOOGLE_ANALYTICS_ID %}\
+        ga('create', '{{ GOOGLE_ANALYTICS_ID }}', 'auto');\
+        {% endif %}\
+        ga('send', 'pageview');"
+    );
+}
