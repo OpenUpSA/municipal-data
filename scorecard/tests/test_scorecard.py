8113f82a4f7c4084d0529364bd6ac34e93bf4aae
michaelglenister <mickeyjhg@gmail.com>
2022-01-31 20:44:04 +0200

    Use existing resources to populate the test db

--- scorecard/tests/test_scorecard.py
+++ scorecard/tests/test_scorecard.py
@@ -1,20 +1,60 @@
-import json
 from django.contrib.sites.models import Site
 from django.core.management import call_command
 
-from django.test import (
-    TransactionTestCase,
-    Client,
+from .. import models
+from . import (
+    import_data,
 )
-
+from .resources import (
+    MedianGroupResource,
+    RatingCountGroupResource,
+)
 from municipal_finance.tests.helpers import BaseSeleniumTestCase
 
 
+fixtures = {
+    "geography": {
+        "geo_level": "municipality",
+        "geo_code": "BUF",
+        "name": "Buffalo City",
+        "long_name": "Buffalo City, Eastern Cape",
+        "square_kms": 2751.69154949282,
+        "parent_level": "province",
+        "parent_code": "EC",
+        "province_name": "Eastern Cape",
+        "province_code": "EC",
+        "category": "A",
+        "miif_category": "A",
+        "population": 781026,
+        "postal_address_1": "P O BOX 134",
+        "postal_address_2": "EAST LONDON",
+        "postal_address_3": "5200",
+        "street_address_1": "Trust Bank Centre",
+        "street_address_2": "C/O Oxford & North Street",
+        "street_address_3": "East London",
+        "street_address_4": "5200",
+        "phone_number": "043 705 2000",
+        "fax_number": "043 743 8568",
+        "url": "http://www.buffalocity.gov.za",
+    }
+}
+
 class ScorecardTest(BaseSeleniumTestCase):
-    fixtures = ["seeddata", "demo-data", "compiled_profile"]
+    fixtures = ["seeddata", "compiled_profile"]
 
     def setUp(self):
         super(ScorecardTest, self).setUp()
+
+        self.geography = models.Geography.objects.create(
+            **fixtures["geography"])
+        import_data(
+            MedianGroupResource,
+            "views/median_group.csv",
+        )
+        import_data(
+            RatingCountGroupResource,
+            "views/rating_count_group.csv",
+        )
         Site.objects.filter(id=2).update(domain='municipalmoney.org.za', name='Scorecard')
 
     def test_scorecard_formula(self):
@@ -41,20 +81,3 @@ class ScorecardTest(BaseSeleniumTestCase):
         #selenium.get("%s%s" % (self.live_server_url, "/profiles/municipality-BUF-buffalo-city/"))
         #is_displayed = selenium.find_element_by_css_selector('.is--pre-2019-20').is_displayed()
         #assert is_displayed == False
-
-
-class GeographyDetailViewTestCase(TransactionTestCase):
-    serialized_rollback = True
-
-    def test_context(self):
-        # Make request
-        client = Client()
-        response = client.get("/profiles/municipality-BUF-buffalo-city/")
-        context = response.context
-        page_data = json.loads(context["page_data_json"])
-        # Test for amount types
-        self.assertIsInstance(page_data["amount_types_v1"], dict)
-        # Test for cube names
-        self.assertIsInstance(page_data["cube_names"], dict)
-        # Test for municipality category descriptions
-        self.assertIsInstance(page_data["municipal_category_descriptions"], dict)
