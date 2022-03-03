2e7ec502b589e090212093f4b4d3273a4a3c963b
michaelglenister <mickeyjhg@gmail.com>
2022-03-03 19:24:11 +0200

    Fix calculation element test

--- scorecard/tests/test_scorecard.py
+++ scorecard/tests/test_scorecard.py
@@ -1,5 +1,4 @@
 from django.contrib.sites.models import Site
-from django.core.management import call_command
 
 from municipal_finance.tests.helpers import BaseSeleniumTestCase
 
@@ -12,7 +11,7 @@ class ScorecardTest(BaseSeleniumTestCase):
         super(ScorecardTest, self).setUp()
         Site.objects.filter(id=2).update(domain='municipalmoney.org.za', name='Scorecard')
 
-    def test_formulas(self):
+    def test_calculation_element(self):
         selenium = self.selenium
         selenium.get("%s%s" % (self.live_server_url, "/profiles/municipality-BUF-buffalo-city/"))
 
@@ -24,18 +23,7 @@ class ScorecardTest(BaseSeleniumTestCase):
         selenium.find_elements_by_css_selector(".expand-block")[3].click()
 
         self.wait_until_text_in(".indicator-metric__value", "R1 823 063 888")
-        self.wait_until_text_in(".indicator-calculation__formula-text", "Cash available at year end")
-        self.wait_until_text_in(".is--post-2019-20", "item code 0430, Audited Actual")
-        self.wait_until_text_in(".is--pre-2019-20", "item code 4200, Audited Actual")
 
-    def test_data_explorer_links(self):
-        selenium = self.selenium
-        selenium.get("%s%s" % (self.live_server_url, "/profiles/municipality-BUF-buffalo-city/"))
-
-        selenium.find_elements_by_css_selector(".expand-block")[3].click()
-
-        element = selenium.find_element_by_css_selector('.is--post-2019-20 a').get_attribute("href")
-        assert element == "http://portal:8002/table/cflow_v2/?municipalities=BUF&year=2019&items=4200&amountType=AUDA"
-
-        element = selenium.find_element_by_css_selector('.is--pre-2019-20 a').get_attribute("href")
-        assert element == "http://portal:8002/table/cflow/?municipalities=BUF&year=2019&items=4200&amountType=AUDA"
+        element = selenium.find_elements_by_css_selector('.expand-block__content_inner')[3].get_attribute("innerHTML")
+        self.assertIn('[Cash Flow] item code 4200, Audited Actual', element)
+        self.assertIn('http://portal:8002/', element)
