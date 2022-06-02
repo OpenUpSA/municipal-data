aa01b9481c24b770a6574031ef373684818b6ab1
michaelglenister <mickeyjhg@gmail.com>
2022-06-02 11:10:10 +0200

    Hide local income sources

--- scorecard/static/js/charts.js
+++ scorecard/static/js/charts.js
@@ -1,5 +1,6 @@
 var formatLocale = d3_format.formatLocale({decimal: ".", thousands: " ", grouping: [3], currency: ["R", ""]});
 
+$('#local-income-sources').hide();
 var formats = {
   currency: formatLocale.format("$,.0f"),
   percent: function(n) {
