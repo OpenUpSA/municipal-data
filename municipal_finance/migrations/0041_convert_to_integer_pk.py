a308a3f57567be4b1fbe69edb709c0cd80475990
michaelglenister <mickeyjhg@gmail.com>
2022-08-24 19:20:51 +0200

    Index fact and item keys

--- municipal_finance/migrations/0041_convert_to_integer_pk.py
+++ municipal_finance/migrations/0041_convert_to_integer_pk.py
@@ -1,5 +1,6 @@
 from django.db import migrations, models
 import municipal_finance.models.small_auto_field
+import django.db.models.deletion
 
 
 def clone_items_table(apps, schema_editor):
@@ -16,12 +17,12 @@ def link_new_indexes(apps, schema_editor):
     items_model = apps.get_model("municipal_finance", "AgedCreditorItemsV1Migrate")
     facts_model = apps.get_model("municipal_finance", "AgedCreditorFactsV1")
     db_alias = schema_editor.connection.alias
-    items = items_model.objects.using(db_alias).all()
     facts = facts_model.objects.using(db_alias).all()
 
     for fact in facts:
-        # Get corresponding item id and save
-
+        item = items_model.objects.using(db_alias).get(code=fact.item_code.code)
+        fact.item=item
+        fact.save()
 
 class Migration(migrations.Migration):
 
@@ -49,6 +50,12 @@ class Migration(migrations.Migration):
                 'db_table': 'aged_creditor_items',
             },
         ),
+        migrations.AddField(
+            model_name='agedcreditorfactsv1',
+            name='item',
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.DO_NOTHING, to='municipal_finance.AgedCreditorItemsV1Migrate'),
+            preserve_default=False,
+        ),
         migrations.RunPython(clone_items_table),
-        #migrations.RunPython(link_new_indexes),
+        migrations.RunPython(link_new_indexes),
     ]
