be49f0837a4280b958798aee26f2667010a1afd6
michaelglenister <mickeyjhg@gmail.com>
2022-10-06 09:57:06 +0200

    Test deploy

--- bin/railway-deploy.sh
+++ bin/railway-deploy.sh
@@ -7,4 +7,4 @@ set -o nounset
 
 python manage.py collectstatic --noinput
 python manage.py migrate --no-input
-gunicorn  --limit-request-line 8190 --worker-class gevent municipal_finance.wsgi:application --log-file - --bind 0.0.0.0:${PORT:-5000} --timeout 480
+#gunicorn  --limit-request-line 8190 --worker-class gevent municipal_finance.wsgi:application --log-file - --bind 0.0.0.0:${PORT:-5000} --timeout 480
