628229b012687c168006502dc82dbc0d36796627
michaelglenister <mickeyjhg@gmail.com>
2022-10-06 14:58:32 +0200

    Cube loading

--- bin/railway-deploy.sh
+++ bin/railway-deploy.sh
@@ -6,5 +6,7 @@ set -o nounset
 
 
 python manage.py collectstatic --noinput
-PRELOAD_CUBES=false python manage.py migrate --no-input
+export PRELOAD_CUBES=false
+python manage.py migrate --no-input
+export PRELOAD_CUBES=true
 gunicorn  --limit-request-line 8190 --worker-class gevent municipal_finance.wsgi:application --log-file - --bind 0.0.0.0:${PORT:-5000} --timeout 480
