ad27f1615545c05e6ce423dc6daa2c53a2352d85
michaelglenister <mickeyjhg@gmail.com>
2022-10-06 11:44:06 +0200

    Don't preload cubes when migrating

--- bin/railway-deploy.sh
+++ bin/railway-deploy.sh
@@ -6,5 +6,5 @@ set -o nounset
 
 
 python manage.py collectstatic --noinput
-python manage.py migrate --no-input
-#gunicorn  --limit-request-line 8190 --worker-class gevent municipal_finance.wsgi:application --log-file - --bind 0.0.0.0:${PORT:-5000} --timeout 480
+PRELOAD_CUBES=false python manage.py migrate --no-input
+gunicorn  --limit-request-line 8190 --worker-class gevent municipal_finance.wsgi:application --log-file - --bind 0.0.0.0:${PORT:-5000} --timeout 480
