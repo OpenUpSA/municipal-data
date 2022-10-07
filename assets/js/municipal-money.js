4f449b25105a7e4afe0f3f1562f961460ee8eae8
michaelglenister <mickeyjhg@gmail.com>
2022-10-07 15:25:07 +0200

    Format files

--- assets/js/municipal-money.js
+++ assets/js/municipal-money.js
@@ -4,35 +4,35 @@ import LocatePage from './pages/locate.js';
 import HelpPage from './pages/help.js';
 import './polyfills/custom-event';
 
-$(function() {
-  const pageType = $("body").data("page");
+$(() => {
+  const pageType = $('body').data('page');
   const pageJSON = document.getElementById('page-data').textContent;
   const pageData = pageJSON ? JSON.parse(pageJSON) : null;
 
   switch (pageType) {
-  case "home":
-    new HomePage(pageData);
-    break;
-  case "municipality-profile":
-    new ProfilePage(pageData);
-    break;
-  case "locate":
-    new LocatePage(pageData);
-    break;
-  case "terms":
-    break;
-  case "help":
-    new HelpPage();
-    break;
-  default:
-    console.error("No class for page type: ", pageType);
+    case 'home':
+      new HomePage(pageData);
+      break;
+    case 'municipality-profile':
+      new ProfilePage(pageData);
+      break;
+    case 'locate':
+      new LocatePage(pageData);
+      break;
+    case 'terms':
+      break;
+    case 'help':
+      new HelpPage();
+      break;
+    default:
+      console.error('No class for page type: ', pageType);
   }
 });
 
-$("#Municipality-Search-Hero, #municipality-search-2").keypress(function(e){
-  if(e.keyCode === 13){
+$('#Municipality-Search-Hero, #municipality-search-2').keypress((e) => {
+  if (e.keyCode === 13) {
     e.preventDefault();
   }
 });
 
-window.testSentry = () => nonExistentFunction("Something");
+window.testSentry = () => nonExistentFunction('Something');
