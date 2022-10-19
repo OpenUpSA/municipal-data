fe75838cf06d5985d9f933a189f7ed54b62d5e49
michaelglenister <mickeyjhg@gmail.com>
2022-10-19 15:18:34 +0200

    Migrate for deploy preview

--- bin/start-web.sh
+++ bin/start-web.sh
@@ -4,6 +4,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-
+export PRELOAD_CUBES=false
+python manage.py migrate --no-input
+export PRELOAD_CUBES=true
 python manage.py collectstatic --noinput
 gunicorn  --limit-request-line 8190 --worker-class gevent municipal_finance.wsgi:application --log-file - --bind 0.0.0.0:${PORT:-5000}
