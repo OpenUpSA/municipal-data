7327228527687fa396590a3cbe0c0764300b7cea
michaelglenister <mickeyjhg@gmail.com>
2022-12-05 13:23:35 +0200

    Tweak

--- bin/start-web.sh
+++ bin/start-web.sh
@@ -4,5 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+
 python manage.py collectstatic --noinput
 gunicorn  --limit-request-line 8190 --worker-class gevent municipal_finance.wsgi:application --log-file - --bind 0.0.0.0:${PORT:-5000}
