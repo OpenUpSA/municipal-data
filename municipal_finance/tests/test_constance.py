67bbe90568df6ad46fc228439520458ccf24f27f
michaelglenister <mickeyjhg@gmail.com>
2023-01-27 20:28:53 +0200

    Add test

--- municipal_finance/tests/test_constance.py
+++ municipal_finance/tests/test_constance.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.contrib.sites.models import Site
-from constance import config
 from constance.test import override_config
 
 from municipal_finance.admin import CustomConfigForm
@@ -16,9 +15,5 @@ class TestConstance(TestCase):
 
     def test_default_config(self):
         form = CustomConfigForm(initial={})
-        print(form)
-        self.assertEqual(config.CAPITAL_PROJECT_SUMMARY_YEAR, "2019/2020")
-
-    def test_config_change(self):
-        with override_config(CAPITAL_PROJECT_SUMMARY_YEAR="2020/2021"):
-            self.assertEqual(config.CAPITAL_PROJECT_SUMMARY_YEAR, "2020/2021")
+        choices = form.fields["CAPITAL_PROJECT_SUMMARY_YEAR"].choices
+        self.assertEqual(choices, [('2019/2020', '2019/2020')])
