ea90f5ee6778aa0bb4c382df4191f60b8d1ffb5f
michaelglenister <mickeyjhg@gmail.com>
2023-02-03 13:13:40 +0200

    Add test constance test for additional financial year

--- municipal_finance/tests/test_constance.py
+++ municipal_finance/tests/test_constance.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.contrib.sites.models import Site
-from constance.test import override_config
 
 from municipal_finance.admin import CustomConfigForm
+from infrastructure.models import FinancialYear
+
 
 class TestConstance(TestCase):
     fixtures = ["seeddata", "demo-data"]
@@ -16,4 +17,12 @@ class TestConstance(TestCase):
     def test_default_config(self):
         form = CustomConfigForm(initial={})
         choices = form.fields["CAPITAL_PROJECT_SUMMARY_YEAR"].choices
-        self.assertEqual(choices, [('2019/2020', '2019/2020'), ('2020/2021', '2020/2021')])
+        self.assertEqual(choices, [("2019/2020", "2019/2020")])
+
+    def test_additinoal_year(self):
+        fy = FinancialYear.objects.create(budget_year="2020/2021")
+        form = CustomConfigForm(initial={})
+        choices = form.fields["CAPITAL_PROJECT_SUMMARY_YEAR"].choices
+        self.assertEqual(
+            choices, [("2019/2020", "2019/2020"), ("2020/2021", "2020/2021")]
+        )
