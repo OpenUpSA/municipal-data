510a35324d1ff6f4d77bc2152932c4db1e27f7b6
michaelglenister <mickeyjhg@gmail.com>
2023-02-09 19:27:08 +0200

    Add test for infra facet count

--- infrastructure/tests/test_api_views.py
+++ infrastructure/tests/test_api_views.py
@@ -6,7 +6,8 @@ class TestProject(TestCase):
 
     def test_infrastructure_project_filters(self):
         response = self.client.get(
-            "/api/v1/infrastructure/search/?q=&province=Western+Cape&municipality=City+of+Cape+Town&project_type=New&function=Administrative+and+Corporate+Support&budget_phase=Budget+year&quarterly_phase=Original+Budget&financial_year=2019%2F2020&ordering=-total_forecast_budget")
+            "/api/v1/infrastructure/search/?q=&province=Western+Cape&municipality=City+of+Cape+Town&project_type=New&function=Administrative+and+Corporate+Support&budget_phase=Budget+year&quarterly_phase=Original+Budget&financial_year=2019%2F2020&ordering=-total_forecast_budget"
+        )
         self.assertEqual(response.status_code, 200)
         js = response.json()
         self.assertEquals(js["count"], 2)
@@ -14,15 +15,26 @@ class TestProject(TestCase):
 
     def test_infrastructure_project_search(self):
         response = self.client.get(
-            "/api/v1/infrastructure/search/?q=PC001002004002_00473&budget_phase=Budget+year&quarterly_phase=Original+Budget&financial_year=2019%2F2020&ordering=-total_forecast_budget")
+            "/api/v1/infrastructure/search/?q=PC001002004002_00473&budget_phase=Budget+year&quarterly_phase=Original+Budget&financial_year=2019%2F2020&ordering=-total_forecast_budget"
+        )
         self.assertEqual(response.status_code, 200)
         js = response.json()
         self.assertEquals(js["count"], 1)
         self.assertEquals(len(js["results"]["projects"]), 1)
 
         response = self.client.get(
-            "/api/v1/infrastructure/search/?q=Acquisition&budget_phase=Budget+year&quarterly_phase=Original+Budget&financial_year=2019%2F2020&ordering=-total_forecast_budget")
+            "/api/v1/infrastructure/search/?q=Acquisition&budget_phase=Budget+year&quarterly_phase=Original+Budget&financial_year=2019%2F2020&ordering=-total_forecast_budget"
+        )
         self.assertEqual(response.status_code, 200)
         js = response.json()
         self.assertEquals(js["count"], 1)
         self.assertEquals(len(js["results"]["projects"]), 1)
+
+    def test_facet_count(self):
+        response = self.client.get(
+            "/api/v1/infrastructure/search/?q=&budget_phase=Budget+year&financial_year=2019%2F2020"
+        )
+        self.assertEqual(response.status_code, 200)
+
+        js = response.json()
+        self.assertEquals(js["results"]["facets"]["municipality"][0]["count"], 2)
