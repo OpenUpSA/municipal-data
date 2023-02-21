cd1ba6d3d00c1573fdab382dca9fd47ecbef7ace
michaelglenister <mickeyjhg@gmail.com>
2023-02-21 18:41:05 +0200

    Lint

--- assets/js/pages/help.js
+++ assets/js/pages/help.js
@@ -9,42 +9,43 @@ export default class HelpPage {
 }
 
 const videos = {
-  "Introduction to Municapal Finances": {
-    "description": "short description", "files": [
-      { "language": "English", "size": ["100"] },
-      { "language": "Afrikaans", "size": ["110"] },
-    ]
+  'Introduction to Municapal Finances': {
+    description: 'short description',
+    files: [
+      { language: 'English', size: ['100'] },
+      { language: 'Afrikaans', size: ['110'] },
+    ],
   },
 };
-const infoVideo = $("#training .sub-section");
-const title = ".informational-video_title";
-const desc = ".informational-video_info p"
-const drCurrentLang = ".language-dropdown .dropdown__current-select";
-const drCurrentSize = ".size-dropdown .dropdown__current-select";
-const dropdownLangItems = ".language-dropdown .dropdown-list";
-const dropdownSizeItems = ".size-dropdown .dropdown-list";
-let dropdownItem = $("<a href='#' data-option='none' class='dropdown-link dropdown-link--current w-dropdown-link' tabindex='0'></a>");
+const infoVideo = $('#training .sub-section');
+const title = '.informational-video_title';
+const desc = '.informational-video_info p';
+const drCurrentLang = '.language-dropdown .dropdown__current-select';
+const drCurrentSize = '.size-dropdown .dropdown__current-select';
+const dropdownLangItems = '.language-dropdown .dropdown-list';
+const dropdownSizeItems = '.size-dropdown .dropdown-list';
+const dropdownItem = $("<a href='#' data-option='none' class='dropdown-link dropdown-link--current w-dropdown-link' tabindex='0'></a>");
 
-//$(".informational-video_block:first").hide();
-//$(dropdownSizeItems).empty();
-//$(dropdownSizeItems).empty();
+// $(".informational-video_block:first").hide();
+// $(dropdownSizeItems).empty();
+// $(dropdownSizeItems).empty();
 
 $.each(videos, function (key, value) {
-  var videoBlock = $(".informational-video_block:first").clone();
+  var videoBlock = $('.informational-video_block:first').clone();
   videoBlock.find(title).text(key);
   videoBlock.find(desc).text(this.description);
-  //videoBlock.find(drCurrentLang).text("");
-  //videoBlock.find(drCurrentSize).text("");
+  // videoBlock.find(drCurrentLang).text("");
+  // videoBlock.find(drCurrentSize).text("");
 
-  $.each(this.files, function (index, file) {
+  $.each(this.files, (index, file) => {
     console.log(file);
     dropdownItem.text(file.language);
     videoBlock.find(dropdownLangItems).append(dropdownItem);
 
-    /*$.each(file.size, function (index, size) {
+    /* $.each(file.size, function (index, size) {
       dropdownItem.text(size);
       videoBlock.find(dropdownSizeItems).append(dropdownItem);
-    });*/
+    }); */
   });
   videoBlock.appendTo(infoVideo);
 });
