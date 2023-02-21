91605d112584e375e2e56d47b352bb3e5bdce489
michaelglenister <mickeyjhg@gmail.com>
2023-02-21 18:39:01 +0200

    Populate help info video section

--- assets/js/pages/help.js
+++ assets/js/pages/help.js
@@ -14,42 +14,37 @@ const videos = {
       { "language": "English", "size": ["100"] },
       { "language": "Afrikaans", "size": ["110"] },
     ]
-  }
+  },
 };
 const infoVideo = $("#training .sub-section");
 const title = ".informational-video_title";
 const desc = ".informational-video_info p"
 const drCurrentLang = ".language-dropdown .dropdown__current-select";
-const drCurrentSize = ".size-dropdown .dropdown__current-select:last";
-const drLangList = ".language-dropdown .dropdown-list";
-const drSizeList = ".size-dropdown .dropdown-list";
-let drItem = $("<a href='#' data-option='none' class='dropdown-link dropdown-link--current w-dropdown-link' tabindex='0'></a>");
+const drCurrentSize = ".size-dropdown .dropdown__current-select";
+const dropdownLangItems = ".language-dropdown .dropdown-list";
+const dropdownSizeItems = ".size-dropdown .dropdown-list";
+let dropdownItem = $("<a href='#' data-option='none' class='dropdown-link dropdown-link--current w-dropdown-link' tabindex='0'></a>");
 
 //$(".informational-video_block:first").hide();
-$(drLangList).empty();
-$(drSizeList).empty();
+//$(dropdownSizeItems).empty();
+//$(dropdownSizeItems).empty();
 
 $.each(videos, function (key, value) {
-  //console.log(this);
   var videoBlock = $(".informational-video_block:first").clone();
   videoBlock.find(title).text(key);
   videoBlock.find(desc).text(this.description);
   //videoBlock.find(drCurrentLang).text("");
   //videoBlock.find(drCurrentSize).text("");
 
-  $.each(this.files, function (index, item) {
-    console.log(item);
-    drItem.text(item.language);
-    videoBlock.find(drLangList).append(drItem);
+  $.each(this.files, function (index, file) {
+    console.log(file);
+    dropdownItem.text(file.language);
+    videoBlock.find(dropdownLangItems).append(dropdownItem);
 
-    $.each(item.size, function (index, size) {
-      drItem.text(size);
-      videoBlock.find(drSizeList).append(drItem);
-    });
+    /*$.each(file.size, function (index, size) {
+      dropdownItem.text(size);
+      videoBlock.find(dropdownSizeItems).append(dropdownItem);
+    });*/
   });
   videoBlock.appendTo(infoVideo);
 });
-
-/*var videoBlock = $(".informational-video_block:first").clone();
-videoBlock.find('.informational-video_title').text('Video two');
-videoBlock.appendTo(infoVideo);*/
