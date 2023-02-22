15753a6d84cf720fa40db37fd7a0037e7fbed53c
michaelglenister <mickeyjhg@gmail.com>
2023-02-22 14:29:29 +0200

    Populate help video section

--- assets/js/pages/help.js
+++ assets/js/pages/help.js
@@ -1,12 +1,25 @@
 import { focusBlock } from '../utils.js';
+import ReactDOM from 'react-dom'
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 export default class HelpPage {
   constructor() {
     if (location.hash) {
       focusBlock(location.hash.substr(1));
     }
+
+    const ReactBtn = () => (
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    );
+    ReactDOM.render(<ReactBtn />, document.getElementById("react-test"));
+
   }
 }
+
+
 
 const videos = {
   'Introduction to Municapal Finances': {
@@ -17,6 +30,7 @@ const videos = {
     ],
   },
 };
+
 const infoVideo = $('#training .sub-section');
 const title = '.informational-video_title';
 const desc = '.informational-video_info p';
@@ -25,10 +39,11 @@ const drCurrentSize = '.size-dropdown .dropdown__current-select';
 const dropdownLangItems = '.language-dropdown .dropdown-list';
 const dropdownSizeItems = '.size-dropdown .dropdown-list';
 const dropdownItem = $("<a href='#' data-option='none' class='dropdown-link dropdown-link--current w-dropdown-link' tabindex='0'></a>");
+const dropdownItem2 = "<a href='#' data-option='none' class='dropdown-link dropdown-link--current w-dropdown-link' tabindex='0'>";
 
 // $(".informational-video_block:first").hide();
-// $(dropdownSizeItems).empty();
-// $(dropdownSizeItems).empty();
+$(dropdownLangItems).empty();
+$(dropdownSizeItems).empty();
 
 $.each(videos, function (key, value) {
   var videoBlock = $('.informational-video_block:first').clone();
@@ -36,16 +51,17 @@ $.each(videos, function (key, value) {
   videoBlock.find(desc).text(this.description);
   // videoBlock.find(drCurrentLang).text("");
   // videoBlock.find(drCurrentSize).text("");
+  let appendLang = "";
+  let appendSize = "";
 
   $.each(this.files, (index, file) => {
-    console.log(file);
-    dropdownItem.text(file.language);
-    videoBlock.find(dropdownLangItems).append(dropdownItem);
-
-    /* $.each(file.size, function (index, size) {
-      dropdownItem.text(size);
-      videoBlock.find(dropdownSizeItems).append(dropdownItem);
-    }); */
+    appendLang += dropdownItem2 + file.language + "</a>";
+    $.each(file.size, function (index, size) {
+      appendSize += dropdownItem2 + size + "</a>";
+    });
+    videoBlock.find(dropdownSizeItems).append(appendSize);
   });
+
+  videoBlock.find(dropdownLangItems).append(appendLang);
   videoBlock.appendTo(infoVideo);
 });
