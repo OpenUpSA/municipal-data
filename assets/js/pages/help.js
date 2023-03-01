a7e2db7f5b4fdc0c592424b9ed54c5599df5cac4
michaelglenister <mickeyjhg@gmail.com>
2023-03-01 11:58:40 +0200

    Add videos to json blob

--- assets/js/pages/help.js
+++ assets/js/pages/help.js
@@ -13,64 +13,67 @@ const videos = {
     description: "",
     embed: "HeQiX_e8ubg",
     options: [
-      { language: "English", files: { "61.3": "Municipal+Money%3A+Intro+to+Municipal+Finance+English.webm", "1.0": "asdf.webm" } },
-      { language: "Afrikaans", files: { "94.1": "Municipal+Money%3A+Intro+to+Municipal+Finance+Afrikaans.mkv", } },
+      { language: "English", files: { "66.9": "Municipal_Money%3A_Intro_to_Municipal_Finance_English.mp4", "27.7": "compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BEnglish.mp4" } },
+      { language: "isiXhosa", files: { "73.2": "Municipal_Money%3A_Intro_to_Municipal_Finance_isiXhosa.mp4", "30.4": "compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BisiXhosa.mp4" } },
+      { language: "Afrikaans", files: { "82.5": "Municipal_Money%3A_Intro_to_Municipal_Finance_Afrikaans.mp4", "27.8": "compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BAfrikaans.mp4" } },
+      { language: "Sotho", files: { "76.9": "Municipal_Money%3A_Intro_to_Municipal_Finance_Sotho.mp4", "27.7": "compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BSotho.mp4" } },
+      { language: "Zulu", files: { "87.2": "Municipal_Money%3A_Intro_to_Municipal_Finance_Zulu.mp4", "31.3": "compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BZulu.mp4" } },
     ],
   },
   "Irregular, Fruitless and Wasteful Expenditure": {
     description: "",
     embed: "WVZBVJTh0u0",
     options: [
-      { language: "English", files: { "25.5": "Municipal+Money%3A+Irregular%2C+Fruitless+and+Wasteful+Expenditure.mkv", } },
+      { language: "English", files: { "22.7": "Municipal_Money%3A_Irregular%2C_Fruitless_and_Wasteful_Expenditure.mp4", "9.0": "compressed/Municipal%2BMoney_%2BIrregular%2C%2BFruitless%2Band%2BWasteful%2BExpenditure.mp4" } },
     ],
   },
   "Liquidity": {
     description: "",
     embed: "6WUDTN7kBZI",
     options: [
-      { language: "English", files: { "14.9": "Municipal+Money%3A+Liquidity.mkv", } },
+      { language: "English", files: { "13.6": "Municipal_Money%3A_Liquidity.mp4", "10.7": "compressed/Municipal%2BMoney_%2BLiquidity.mp4" } },
     ],
   },
   "Sources of Income": {
     description: "",
     embed: "zb2Wph6Mbpo",
     options: [
-      { language: "English", files: { "4.9": "Municipal+Money%3A+Sources+of+Income.webm", } },
+      { language: "English", files: { "6.0": "Municipal_Money%3A_Sources_of_Income.mp4", "17.8": "compressed/Municipal%2BMoney_%2BSources%2Bof%2BIncome.mp4" } },
     ],
   },
   "Spending of the Capital Budget": {
     description: "",
     embed: "L7rfUkK5PJI",
     options: [
-      { language: "English", files: { "9.2": "Municipal+Money%3A+Spending+of+the+Capital+Budget.webm", } },
+      { language: "English", files: { "12.5": "Municipal_Money%3A_Spending_of_the_Capital_Budget.mp4", "4.5": "compressed/Municipal%2BMoney_%2BSpending%2Bof%2Bthe%2BCapital%2BBudget.mp4" } },
     ],
   },
   "Spending of the Operating Budget": {
     description: "",
     embed: "r8_W4Yn0Oz8",
     options: [
-      { language: "English", files: { "10.3": "Municipal+Money%3A+Spending+of+the+Operating+Budget.webm", } },
+      { language: "English", files: { "13.8": "Municipal_Money%3A_Spending_of_the_Operating_Budget.mp4", "5.1": "compressed/Municipal%2BMoney_%2BSpending%2Bof%2Bthe%2BOperating%2BBudget.mp4" } },
     ],
   },
   "Spending on Repairs & Maintenance": {
     description: "",
     embed: "f2CdUnsEBXA",
     options: [
-      { language: "English", files: { "10.2": "Municipal+Money%3A+Spending+on+Repairs+%26+Maintenance.webm", } },
+      { language: "English", files: { "11.8": "Municipal_Money%3A_Spending_on_Repairs_%26_Maintenance.mp4", "4.9": "compressed/Municipal%2BMoney_%2BSpending%2Bon%2BRepairs%2B_%2BMaintenance.mp4" } },
     ],
   },
   "Cash Balances and Cash Coverage": {
     description: "",
     embed: "-sGcopgP4u0",
     options: [
-      { language: "English", files: { "16.3": "Municipal+Money%3A+Cash+Balances+and+Cash+Coverage.webm", } },
+      { language: "English", files: { "22.5": "Municipal_Money%3A_Cash_Balances_and_Cash_Coverage.mp4", "7.0": "compressed/Municipal%2BMoney_%2BCash%2BBalances%2Band%2BCash%2BCoverage.mp4" } },
     ],
   },
   "Debtors' Collections Ratio": {
     description: "",
     embed: "A15Fvwcx_OY",
     options: [
-      { language: "English", files: { "13.1": "Municipal+Money+Debtors'+Collections+Ratio.mkv", } },
+      { language: "English", files: { "11.8": "Municipal_Money%3A_Debtors'_Collections_Ratio.mp4", "4.5": "compressed/Municipal%2BMoney%2BDebtors_%2BCollections%2BRatio.mp4" } },
     ],
   },
 };
