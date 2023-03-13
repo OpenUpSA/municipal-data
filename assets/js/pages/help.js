a0b4c9cfb506d610c6d1bbf56d089f549c1f405b
michaelglenister <mickeyjhg@gmail.com>
2023-03-13 14:31:03 +0200

    Adjust size label

--- assets/js/pages/help.js
+++ assets/js/pages/help.js
@@ -28,11 +28,11 @@ const videos = {
       ['Zulu', 'zulu'],
     ],
     files: {
-      eng: [['Full size 66.9 MB', 'Municipal_Money%3A_Intro_to_Municipal_Finance_English.mp4'], ['Small size 27.7 MB', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BEnglish.mp4']],
-      xhosa: [['Full size 73.2 MB', 'Municipal_Money%3A_Intro_to_Municipal_Finance_isiXhosa.mp4'], ['Small size 30.4 MB', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BisiXhosa.mp4']],
-      afr: [['Full size 82.5 MB', 'Municipal_Money%3A_Intro_to_Municipal_Finance_Afrikaans.mp4'], ['Small size 27.8 MB', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BAfrikaans.mp4']],
-      sotho: [['Full size 76.9 MB', 'Municipal_Money%3A_Intro_to_Municipal_Finance_Sotho.mp4'], ['Small size 27.7 MB', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BSotho.mp4']],
-      zulu: [['Full size 87.2 MB', 'Municipal_Money%3A_Intro_to_Municipal_Finance_Zulu.mp4'], ['Small size 31.3 MB', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BZulu.mp4']],
+      eng: [['66.9 MB (Full size)', 'Municipal_Money%3A_Intro_to_Municipal_Finance_English.mp4'], ['27.7 MB (Small size)', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BEnglish.mp4']],
+      xhosa: [['73.2 MB (Full size)', 'Municipal_Money%3A_Intro_to_Municipal_Finance_isiXhosa.mp4'], ['30.4 MB (Small size)', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BisiXhosa.mp4']],
+      afr: [['82.5 MB (Full size)', 'Municipal_Money%3A_Intro_to_Municipal_Finance_Afrikaans.mp4'], ['27.8 MB (Small size)', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BAfrikaans.mp4']],
+      sotho: [['76.9 MB (Full size)', 'Municipal_Money%3A_Intro_to_Municipal_Finance_Sotho.mp4'], ['27.7 MB (Small size)', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BSotho.mp4']],
+      zulu: [['87.2 MB (Full size)', 'Municipal_Money%3A_Intro_to_Municipal_Finance_Zulu.mp4'], ['31.3 MB (Small size)', 'compressed/Municipal%2BMoney_%2BIntro%2Bto%2BMunicipal%2BFinance%2BZulu.mp4']],
     },
     sectionMarkers: ['intro'],
   },
@@ -43,7 +43,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 22.7 MB', 'Municipal_Money%3A_Irregular%2C_Fruitless_and_Wasteful_Expenditure.mp4'], ['Small size 9.0 MB', 'compressed/Municipal%2BMoney_%2BIrregular%2C%2BFruitless%2Band%2BWasteful%2BExpenditure.mp4']],
+      eng: [['22.7 MB (Full size)', 'Municipal_Money%3A_Irregular%2C_Fruitless_and_Wasteful_Expenditure.mp4'], ['9.0 MB (Small size)', 'compressed/Municipal%2BMoney_%2BIrregular%2C%2BFruitless%2Band%2BWasteful%2BExpenditure.mp4']],
     },
     sectionMarkers: ['wasteful-expenditure'],
   },
@@ -54,7 +54,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 13.6 MB', 'Municipal_Money%3A_Liquidity.mp4'], ['Small size 10.7 MB', 'compressed/Municipal%2BMoney_%2BLiquidity.mp4']],
+      eng: [['13.6 MB (Full size)', 'Municipal_Money%3A_Liquidity.mp4'], ['10.7 MB (Small size)', 'compressed/Municipal%2BMoney_%2BLiquidity.mp4']],
     },
     sectionMarkers: ['liquidity-ratio', 'current-ratio'],
   },
@@ -65,7 +65,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Small size 6.0 MB', 'Municipal_Money%3A_Sources_of_Income.mp4']],
+      eng: [['6.0 MB (Small size)', 'Municipal_Money%3A_Sources_of_Income.mp4']],
     },
     sectionMarkers: ['income'],
   },
@@ -76,7 +76,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 12.5 MB', 'Municipal_Money%3A_Spending_of_the_Capital_Budget.mp4'], ['Small size 4.5 MB', 'compressed/Municipal%2BMoney_%2BSpending%2Bof%2Bthe%2BCapital%2BBudget.mp4']],
+      eng: [['12.5 MB (Full size)', 'Municipal_Money%3A_Spending_of_the_Capital_Budget.mp4'], ['4.5 MB (Small size)', 'compressed/Municipal%2BMoney_%2BSpending%2Bof%2Bthe%2BCapital%2BBudget.mp4']],
     },
     sectionMarkers: ['capital-budget'],
   },
@@ -87,7 +87,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 13.8 MB', 'Municipal_Money%3A_Spending_of_the_Operating_Budget.mp4'], ['Small size 5.1 MB', 'compressed/Municipal%2BMoney_%2BSpending%2Bof%2Bthe%2BOperating%2BBudget.mp4']],
+      eng: [['13.8 MB (Full size)', 'Municipal_Money%3A_Spending_of_the_Operating_Budget.mp4'], ['5.1 MB (Small size)', 'compressed/Municipal%2BMoney_%2BSpending%2Bof%2Bthe%2BOperating%2BBudget.mp4']],
     },
     sectionMarkers: ['operating-budget'],
   },
@@ -98,7 +98,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 11.8 MB', 'Municipal_Money%3A_Spending_on_Repairs_%26_Maintenance.mp4'], ['Small size 4.9 MB', 'compressed/Municipal%2BMoney_%2BSpending%2Bon%2BRepairs%2B_%2BMaintenance.mp4']],
+      eng: [['11.8 MB (Full size)', 'Municipal_Money%3A_Spending_on_Repairs_%26_Maintenance.mp4'], ['4.9 MB (Small size)', 'compressed/Municipal%2BMoney_%2BSpending%2Bon%2BRepairs%2B_%2BMaintenance.mp4']],
     },
     sectionMarkers: ['repairs-maintenance'],
   },
@@ -109,7 +109,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 22.5 MB', 'Municipal_Money%3A_Cash_Balances_and_Cash_Coverage.mp4'], ['Small size 7.0 MB', 'compressed/Municipal%2BMoney_%2BCash%2BBalances%2Band%2BCash%2BCoverage.mp4']],
+      eng: [['22.5 MB (Full size)', 'Municipal_Money%3A_Cash_Balances_and_Cash_Coverage.mp4'], ['7.0 MB (Small size)', 'compressed/Municipal%2BMoney_%2BCash%2BBalances%2Band%2BCash%2BCoverage.mp4']],
     },
     sectionMarkers: ['cash-balance', 'cash-coverage'],
   },
@@ -120,7 +120,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 11.8 MB', "Municipal_Money%3A_Debtors'_Collections_Ratio.mp4"], ['Small size 4.5 MB', 'compressed/Municipal%2BMoney%2BDebtors_%2BCollections%2BRatio.mp4']],
+      eng: [['11.8 MB (Full size)', "Municipal_Money%3A_Debtors'_Collections_Ratio.mp4"], ['4.5 MB (Small size)', 'compressed/Municipal%2BMoney%2BDebtors_%2BCollections%2BRatio.mp4']],
     },
     sectionMarkers: ['collection-rate'],
   },
@@ -131,7 +131,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 57.4 MB', 'Municipal_Money%3A_Conditional_Grants.mp4'], ['Small size 8.7 MB', 'compressed/Municipal_Money%3A_Conditional_Grants.mp4']],
+      eng: [['57.4 MB (Full size)', 'Municipal_Money%3A_Conditional_Grants.mp4'], ['8.7 MB (Small size)', 'compressed/Municipal_Money%3A_Conditional_Grants.mp4']],
     },
     sectionMarkers: ['grants'],
   },
@@ -142,7 +142,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 129.6 MB', 'Municipal_Money%3A_Household_Bills.mp4'], ['Small size 13.9 MB', 'compressed/Municipal_Money%3A_Household_Bills.mp4']],
+      eng: [['129.6 MB (Full size)', 'Municipal_Money%3A_Household_Bills.mp4'], ['13.9 MB (Small size)', 'compressed/Municipal_Money%3A_Household_Bills.mp4']],
     },
     sectionMarkers: ['household'],
   },
@@ -153,7 +153,7 @@ const videos = {
       ['English', 'eng'],
     ],
     files: {
-      eng: [['Full size 12.2 MB', 'Municipal_Money%3A_Capital_Projects.mp4'], ['Small size 3.0 MB', 'compressed/Municipal_Money%3A_Capital_Projects.mp4']],
+      eng: [['12.2 MB (Full size)', 'Municipal_Money%3A_Capital_Projects.mp4'], ['3.0 MB (Small size)', 'compressed/Municipal_Money%3A_Capital_Projects.mp4']],
     },
     sectionMarkers: ['municipal-budget'],
   },
