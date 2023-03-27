6d017c090f7239075c429ad52a72db5cb7461bfe
michaelglenister <mickeyjhg@gmail.com>
2023-03-27 17:50:08 +0200

    Check dropdown and project count
    This check has been done further up in the test but is here to make sure
    selenium waits for the page to load fully before proceeding

--- infrastructure/tests/test_search_page.py
+++ infrastructure/tests/test_search_page.py
@@ -380,9 +380,11 @@ class CapitalSearchTest(BaseSeleniumTestCase):
         self.wait_until_text_in(".search-detail_projects", "2")
         self.wait_until_text_in("#functions-dropdown .text-block", "All Functions")
 
-        # Add and remove a filter with a dropdown menu
+        # Add and also remove a filter with a dropdown menu before clicking back
         self.click("#functions-dropdown .chart-dropdown_trigger")
         self.selenium.find_elements_by_css_selector("#functions-dropdown .chart-dropdown_list a")[1].click()
+        self.wait_until_text_in(".search-detail_projects", "1")
+        self.wait_until_text_in("#functions-dropdown .text-block", "Administrative and Corporate Support")
         self.click("#functions-dropdown .chart-dropdown_trigger")
         self.selenium.find_elements_by_css_selector("#functions-dropdown .chart-dropdown_list a")[0].click()
 
