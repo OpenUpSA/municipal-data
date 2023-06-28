ca47c3afcc99eabd892407a153cc9c60bde93586
michaelglenister <mickeyjhg@gmail.com>
2023-06-28 17:32:45 +0200

    Uploading to S3

--- municipal_finance/storage.py
+++ municipal_finance/storage.py
@@ -1,6 +1,14 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+import os
+import boto3
 
 
 class MediaStorage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+
+
+def upload_file(file_name, bucket):
+    s3_client = boto3.client("s3")
+    response = s3_client.upload_file(file_name, bucket, f"{file_name}_object")
+    return True
