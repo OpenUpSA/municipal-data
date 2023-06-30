6fd383b30870934b4d185ea2c6124a68dc182ad4
michaelglenister <mickeyjhg@gmail.com>
2023-06-30 16:48:42 +0200

    Rename file_object

--- municipal_finance/storage.py
+++ municipal_finance/storage.py
@@ -10,14 +10,14 @@ class MediaStorage(S3Boto3Storage):
     file_overwrite = False
 
 
-def upload_object(object, name, bucket):
+def upload_object(file_object, name, bucket):
     s3 = boto3.client(
         "s3",
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
     )
     response = s3.put_object(
-        Body=object,
+        Body=file_object,
         Bucket=bucket,
         Key=f"{name}_object",
     )
