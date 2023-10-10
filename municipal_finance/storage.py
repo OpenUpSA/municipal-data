d47bde024182e77ae619e7b9b54ab2b8409ae2a6
michaelglenister <mickeyjhg@gmail.com>
2023-10-10 17:41:54 +0200

    Updating S3Boto3 config

--- municipal_finance/storage.py
+++ municipal_finance/storage.py
@@ -4,3 +4,5 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaStorage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+    querystring_auth = False
+    region_name = "eu-west-1"
