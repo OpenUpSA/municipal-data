bd5ae49326ccc77a88e0faa546f6410e4683c668
michaelglenister <mickeyjhg@gmail.com>
2023-10-10 18:11:40 +0200

    Remove querystring_auth config

--- municipal_finance/storage.py
+++ municipal_finance/storage.py
@@ -4,5 +4,4 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaStorage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
-    querystring_auth = False
     region_name = "eu-west-1"
