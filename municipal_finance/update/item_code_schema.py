49fec10ca173c1263efcd81532230fa210658289
michaelglenister <mickeyjhg@gmail.com>
2024-04-30 17:20:27 +0200

    Item code tweak

--- municipal_finance/update/item_code_schema.py
+++ municipal_finance/update/item_code_schema.py
@@ -15,22 +15,20 @@ schema_codes = {
     "A4": IncexpItemsV2,
 }
 
-schema_codes_tmp = {
-    "A6": "finpos",
-}
-
 
 def update_item_code_schema(update_obj, batch_size, **kwargs):
     file = default_storage.open(update_obj.file.name, "rb")
     workbook = xlrd.open_workbook(file_contents=file.read())
-    sheet = workbook.sheet_by_index(0)
 
-    for i in range(sheet.nrows):
-        schema_code = sheet.row_values(i)[0].strip()
-        if schema_code != "" and schema_code in schema_codes:
-            label = sheet.row_values(i)[4].strip()
-            code = sheet.row_values(i)[1].strip()
+    for key in schema_codes:
+        sheet = workbook.sheet_by_name(key)
 
-            schema_codes[schema_code].objects.create(
-                code=code, label=label, version=update_obj
-            )
+        for i in range(sheet.nrows):
+            schema_code = sheet.row_values(i)[0].strip()
+            if schema_code != "" and schema_code in schema_codes:
+                label = sheet.row_values(i)[4].strip()
+                code = sheet.row_values(i)[8].strip()
+
+                schema_codes[schema_code].objects.create(
+                    code=code, label=label, version=update_obj
+                )
