4495a60ebba9d4b2a2a4e853f1985512dffd8d13
michaelglenister <mickeyjhg@gmail.com>
2026-05-22 13:38:40 +0200

    Fix ordering of subcategory migration

--- municipal_finance/migrations/0003b_add_incexp_subcategory.py
+++ municipal_finance/migrations/0003b_add_incexp_subcategory.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('municipal_finance', '0005_auto_20240814_1407'),
+        ('municipal_finance', '0003_add_item_codes'),
     ]
 
     operations = [
